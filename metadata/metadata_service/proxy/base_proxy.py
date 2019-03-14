36023d0064e18fb8b4550382a94f7255de30f530
Verdan Mahmood <verdan.mahmood@gmail.com>
2019-03-14 13:36:27 +0100

    Enforce subclasses to define required functions

--- metadata/metadata_service/proxy/base_proxy.py
+++ metadata/metadata_service/proxy/base_proxy.py
@@ -1,6 +1,13 @@
-class BaseProxy:
+from abc import ABCMeta, abstractmethod
+
+from metadata_service.entity.table_detail import Table
+
+
+class BaseProxy(metaclass=ABCMeta):
     """
     Base Proxy, which behaves like an interface for all
     the proxy clients available in the amundsen metadata service
     """
-    pass
+    @abstractmethod
+    def get_table(self, *, table_uri: str) -> Table:
+        pass
