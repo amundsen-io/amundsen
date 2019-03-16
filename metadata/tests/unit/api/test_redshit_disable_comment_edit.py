7d2f58d06418c01af7a50db2ea044b93340b03ca
Verdan Mahmood <verdan.mahmood@gmail.com>
2019-03-16 01:35:54 +0100

    Fix the test cases

--- metadata/tests/unit/api/test_redshit_disable_comment_edit.py
+++ metadata/tests/unit/api/test_redshit_disable_comment_edit.py
@@ -2,24 +2,13 @@ import unittest
 from http import HTTPStatus
 
 from mock import patch
-from metadata_service import proxy
-from metadata_service import create_app
 from metadata_service.api.table import TableDescriptionAPI
 from metadata_service.api.column import ColumnDescriptionAPI
 
 
 class RedshiftCommentEditDisableTest(unittest.TestCase):
-
-    def setUp(self) -> None:
-        self.app = create_app(config_module_class='metadata_service.config.LocalConfig')
-        self.app_context = self.app.app_context()
-        self.app_context.push()
-
-    def tearDown(self):
-        self.app_context.pop()
-
     def test_table_comment_edit(self) -> None:
-        with patch.object(proxy, 'get_proxy_client'):
+        with patch('metadata_service.api.table.get_proxy_client'):
             tbl_dscrpt_api = TableDescriptionAPI()
 
             table_uri = 'hive://gold.test_schema/test_table'
@@ -27,7 +16,7 @@ class RedshiftCommentEditDisableTest(unittest.TestCase):
             self.assertEqual(list(response)[1], HTTPStatus.OK)
 
     def test_column_comment_edit(self) -> None:
-        with patch.object(proxy, 'get_proxy_client'):
+        with patch('metadata_service.api.column.get_proxy_client'):
             col_dscrpt_api = ColumnDescriptionAPI()
 
             table_uri = 'hive://gold.test_schema/test_table'
