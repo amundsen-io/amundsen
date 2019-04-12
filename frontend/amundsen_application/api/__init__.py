52ed0e2b49e666dacdddaa86b0dae9392a3208d0
Jan Kyri <thenaturalist@users.noreply.github.com>
2019-04-13 01:14:00 +0200

    Remove favicon path (#76)
    * update requirements path & favicon path in api
    
    * Update __init__.py
    
    * Remove 'favicon' route
    
    * Update __init__.py

--- frontend/amundsen_application/api/__init__.py
+++ frontend/amundsen_application/api/__init__.py
@@ -1,12 +1,9 @@
-import os
-from typing import Any, IO, Tuple
+from typing import Any, Tuple
 
-from flask import Flask, render_template, send_from_directory
-from flask import current_app as app
+from flask import Flask, render_template
 
 
 def init_routes(app: Flask) -> None:
-    app.add_url_rule('/favicon.ico', 'favicon', favicon)
     app.add_url_rule('/healthcheck', 'healthcheck', healthcheck)
     app.add_url_rule('/', 'index', index, defaults={'path': ''})  # also functions as catch_all
     app.add_url_rule('/<path:path>', 'index', index)  # catch_all
@@ -18,9 +15,3 @@ def index(path: str) -> Any:
 
 def healthcheck() -> Tuple[str, int]:
     return '', 200  # pragma: no cover
-
-
-def favicon() -> IO[bytes]:
-    """ TODO: Design team should provide us with a default icon """
-    return send_from_directory(os.path.join(app.root_path, 'static/images'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')  # pragma: no cover
