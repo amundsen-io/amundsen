97e2e210396290c9968b89fa0b44fdddb0df064d
Verdan Mahmood <verdan.mahmood@gmail.com>
2019-08-06 20:38:41 +0200

    Updates the requirements in order to fix pip package (#46)

--- search/setup.py
+++ search/setup.py
@@ -1,7 +1,12 @@
+import os
+
 from setuptools import setup, find_packages
 
-__version__ = '1.1.6'
+__version__ = '1.1.7'
 
+requirements_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'requirements.txt')
+with open(requirements_path) as requirements_file:
+    requirements = requirements_file.readlines()
 
 setup(
     name='amundsen-search',
@@ -12,12 +17,6 @@ setup(
     maintainer_email='dp-tools@lyft.com',
     packages=find_packages(exclude=['tests*']),
     dependency_links=[],
-    install_requires=[
-        'Flask-RESTFul==0.3.6',
-        'elasticsearch==6.2.0',
-        'elasticsearch-dsl==6.1.0',
-        'statsd==3.2.1',
-        'atlasclient==0.1.7'
-    ],
+    install_requires=requirements,
     python_requires=">=3.6"
 )
