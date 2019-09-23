48bb67f7df2c80abde6c68fdb1b416f3ab591fd3
Stacey Watro <watro.stacey@gmail.com>
2019-09-23 18:17:45 -0400

    Adds swagger documentation. (#59)
    * Adds swagger documentation.
    
    * Puts requirements in alphabetical order. Locks version of jsonschema to 2.6.0 to be compatiable with flasgger.
    
    * Adds swagger unit tests. Remove unneeded fields from yml file.
    
    * Adds documentation about the documentation
    
    * Adds real data documentation for document and user apis. Reorganizes yml files.
    
    * Cleans up documention
    
    * Puts back doc strings.
    
    * Update setup.py

--- search/setup.py
+++ search/setup.py
@@ -2,7 +2,7 @@ import os
 
 from setuptools import setup, find_packages
 
-__version__ = '1.2.1'
+__version__ = '1.3.0'
 
 
 requirements_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'requirements.txt')
@@ -17,6 +17,8 @@ setup(
     maintainer='Lyft',
     maintainer_email='dp-tools@lyft.com',
     packages=find_packages(exclude=['tests*']),
+    include_package_data=True,
+    zip_safe=False,
     dependency_links=[],
     install_requires=requirements,
     python_requires=">=3.6"
