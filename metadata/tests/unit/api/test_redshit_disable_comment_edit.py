55dfb5732537551b8f019c72d17d1310ec8a4b66
Tao Feng <fengtao04@gmail.com>
2019-10-02 11:21:30 -0700

    Change the field to data (#82)

--- metadata/tests/unit/api/test_redshit_disable_comment_edit.py
+++ metadata/tests/unit/api/test_redshit_disable_comment_edit.py
@@ -11,7 +11,7 @@ class RedshiftCommentEditDisableTest(BasicTestCase):
         with patch('metadata_service.api.table.get_proxy_client'):
             table_uri = 'hive://gold.test_schema/test_table'
             url = '/table/' + table_uri + '/description'
-            response = self.app.test_client().put(url, json=json.dumps({'description': 'test table'}))
+            response = self.app.test_client().put(url, data=json.dumps({'description': 'test table'}))
             self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_column_comment_edit(self) -> None:
@@ -19,7 +19,7 @@ class RedshiftCommentEditDisableTest(BasicTestCase):
             table_uri = 'hive://gold.test_schema/test_table'
             column_name = 'foo'
             url = '/table/' + table_uri + '/column/' + column_name + '/description'
-            response = self.app.test_client().put(url, json=json.dumps({'description': 'test column'}))
+            response = self.app.test_client().put(url, data=json.dumps({'description': 'test column'}))
             self.assertEqual(response.status_code, HTTPStatus.OK)
 
 
