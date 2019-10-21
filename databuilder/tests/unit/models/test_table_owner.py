cf8ecc17c6caeb2df3d70b8818fdc1ee3d696e64
Mikhail Ivanov <mikhail.ivanov8@gmail.com>
2019-10-21 08:26:19 +0400

    Add sample table owner data to quick start (#139) (#150)
    * Add sample table owner data to quick start (#139)
    
    * Update example/sample_data/sample_table_owner.csv
    
    Co-Authored-By: jornh <jornhansen@gmail.com>
    
    * Add sample table owner data to quick start

--- databuilder/tests/unit/models/test_table_owner.py
+++ databuilder/tests/unit/models/test_table_owner.py
@@ -3,7 +3,8 @@ from databuilder.models.user import User
 from databuilder.models.table_owner import TableOwner
 
 
-from databuilder.models.neo4j_csv_serde import RELATION_START_KEY, RELATION_START_LABEL, RELATION_END_KEY, \
+from databuilder.models.neo4j_csv_serde import NODE_KEY, NODE_LABEL, \
+    RELATION_START_KEY, RELATION_START_LABEL, RELATION_END_KEY, \
     RELATION_END_LABEL, RELATION_TYPE, RELATION_REVERSE_TYPE
 
 
@@ -11,6 +12,8 @@ db = 'hive'
 SCHEMA = 'BASE'
 TABLE = 'TEST'
 CLUSTER = 'DEFAULT'
+owner1 = 'user1@1'
+owner2 = 'user2@2'
 
 
 class TestTableOwner(unittest.TestCase):
@@ -22,12 +25,12 @@ class TestTableOwner(unittest.TestCase):
                                       schema_name=SCHEMA,
                                       table_name=TABLE,
                                       cluster=CLUSTER,
-                                      owners=['user1@1', 'user2@2'])
+                                      owners="user1@1, UsER2@2 ")
 
     def test_get_owner_model_key(self):
         # type: () -> None
-        owner = self.table_owner.get_owner_model_key('user1@1')
-        self.assertEquals(owner, 'user1@1')
+        owner = self.table_owner.get_owner_model_key(owner1)
+        self.assertEquals(owner, owner1)
 
     def test_get_metadata_model_key(self):
         # type: () -> None
@@ -39,13 +42,35 @@ class TestTableOwner(unittest.TestCase):
         nodes = self.table_owner.create_nodes()
         self.assertEquals(len(nodes), 2)
 
+        node1 = {
+            NODE_KEY: User.USER_NODE_KEY_FORMAT.format(email=owner1),
+            NODE_LABEL: User.USER_NODE_LABEL,
+            User.USER_NODE_EMAIL: owner1
+        }
+        node2 = {
+            NODE_KEY: User.USER_NODE_KEY_FORMAT.format(email=owner2),
+            NODE_LABEL: User.USER_NODE_LABEL,
+            User.USER_NODE_EMAIL: owner2
+        }
+
+        self.assertTrue(node1 in nodes)
+        self.assertTrue(node2 in nodes)
+
     def test_create_relation(self):
         # type: () -> None
         relations = self.table_owner.create_relation()
         self.assertEquals(len(relations), 2)
 
-        relation = {
-            RELATION_START_KEY: 'user1@1',
+        relation1 = {
+            RELATION_START_KEY: owner1,
+            RELATION_START_LABEL: User.USER_NODE_LABEL,
+            RELATION_END_KEY: self.table_owner.get_metadata_model_key(),
+            RELATION_END_LABEL: 'Table',
+            RELATION_TYPE: TableOwner.OWNER_TABLE_RELATION_TYPE,
+            RELATION_REVERSE_TYPE: TableOwner.TABLE_OWNER_RELATION_TYPE
+        }
+        relation2 = {
+            RELATION_START_KEY: owner2,
             RELATION_START_LABEL: User.USER_NODE_LABEL,
             RELATION_END_KEY: self.table_owner.get_metadata_model_key(),
             RELATION_END_LABEL: 'Table',
@@ -53,4 +78,29 @@ class TestTableOwner(unittest.TestCase):
             RELATION_REVERSE_TYPE: TableOwner.TABLE_OWNER_RELATION_TYPE
         }
 
-        self.assertTrue(relation in relations)
+        self.assertTrue(relation1 in relations)
+        self.assertTrue(relation2 in relations)
+
+    def test_create_nodes_with_owners_list(self):
+        # type: () -> None
+        self.table_owner_list = TableOwner(db_name='hive',
+                                           schema_name=SCHEMA,
+                                           table_name=TABLE,
+                                           cluster=CLUSTER,
+                                           owners=['user1@1', ' UsER2@2 '])
+        nodes = self.table_owner_list.create_nodes()
+        self.assertEquals(len(nodes), 2)
+
+        node1 = {
+            NODE_KEY: User.USER_NODE_KEY_FORMAT.format(email=owner1),
+            NODE_LABEL: User.USER_NODE_LABEL,
+            User.USER_NODE_EMAIL: owner1
+        }
+        node2 = {
+            NODE_KEY: User.USER_NODE_KEY_FORMAT.format(email=owner2),
+            NODE_LABEL: User.USER_NODE_LABEL,
+            User.USER_NODE_EMAIL: owner2
+        }
+
+        self.assertTrue(node1 in nodes)
+        self.assertTrue(node2 in nodes)
