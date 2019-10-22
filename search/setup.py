27d8e559b7f7dd0c9612fa69542088d46596f61a
Tao Feng <fengtao04@gmail.com>
2019-10-22 10:26:03 -0700

    Reenable OR operator for ES search result (#69)

--- search/setup.py
+++ search/setup.py
@@ -2,7 +2,7 @@ import os
 
 from setuptools import setup, find_packages
 
-__version__ = '1.4.0'
+__version__ = '1.4.1'
 
 
 requirements_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'requirements.txt')
