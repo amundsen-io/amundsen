5da4036337ceeb6b9368cf376387c1f7018a3414
Jin Hyuk Chang <jinchang@lyft.com>
2020-02-05 13:32:50 -0800

    Temp

--- common/setup.py
+++ common/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='amundsen-common',
-    version='0.1.7',
+    version='0.1.7rc1',
     description='Common code library for Amundsen',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
