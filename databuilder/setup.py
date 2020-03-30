72918ae7eb25ab9f5d2f8398956e32711c6a39ab
Tao Feng <fengtao04@gmail.com>
2020-03-30 12:39:58 -0700

    Make the repo py27 and py3 compatible (#216)

--- databuilder/setup.py
+++ databuilder/setup.py
@@ -44,6 +44,7 @@ setup(
     packages=find_packages(exclude=['tests*']),
     dependency_links=[],
     install_requires=requirements,
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*',
     extras_require={
         ':python_version=="2.7"': ['typing>=3.6'],  # allow typehinting PY2
         'all': all_deps,
