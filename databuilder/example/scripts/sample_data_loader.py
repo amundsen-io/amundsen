a13ca4d1ede2e033b3cf412a2587e8ab53dfc676
Tao Feng <fengtao04@gmail.com>
2020-03-31 10:44:47 -0700

    Fix sample loader for user ES model (#234)

--- databuilder/example/scripts/sample_data_loader.py
+++ databuilder/example/scripts/sample_data_loader.py
@@ -285,7 +285,7 @@ if __name__ == "__main__":
             return user.email as email, user.first_name as first_name, user.last_name as last_name,
             user.full_name as full_name, user.github_username as github_username, user.team_name as team_name,
             user.employee_type as employee_type, manager.email as manager_email, user.slack_id as slack_id,
-            user.is_active as is_active,
+            user.role_name as role_name, user.is_active as is_active,
             REDUCE(sum_r = 0, r in COLLECT(DISTINCT read)| sum_r + r.read_count) AS total_read,
             count(distinct b) as total_own,
             count(distinct c) AS total_follow
