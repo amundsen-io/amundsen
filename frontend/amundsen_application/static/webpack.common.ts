a077d061ee11e2a2f9bee4df4a48e18b490cf094
Marcos Iglesias <190833+Golodhros@users.noreply.github.com>
2020-05-20 18:52:28 -0700

    Adds file caching into our assets (#456)
    * Adds nvmrc file to help sync on node version to our contributors
    
    * Adding FlaskManageWebpack
    
    * Testing a lot of stuff
    
    * Parametrizes webpack configuration
    
    * Cleanup
    
    * Cleaning conflict marks
    
    * Renaming site.webmanifest into manifest.json files
    
    * Adds names to manifest files

--- frontend/amundsen_application/static/webpack.common.ts
+++ frontend/amundsen_application/static/webpack.common.ts
@@ -1,14 +1,31 @@
-import * as path from 'path';
-import * as fs from 'fs';
-import * as webpack from 'webpack';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import * as path from "path";
+import * as fs from "fs";
+import * as webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 // import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer';
-import MomentLocalesPlugin from 'moment-locales-webpack-plugin';
-import {CleanWebpackPlugin} from 'clean-webpack-plugin';
+import MomentLocalesPlugin from "moment-locales-webpack-plugin";
+import {CleanWebpackPlugin} from "clean-webpack-plugin";
 
-import appConfig from './js/config/config';
+import appConfig from "./js/config/config";
 
+const TSX_PATTERN = /\.ts|\.tsx$/;
+const JSX_PATTERN = /\.jsx?$/;
+const CSS_PATTERN = /\.(sa|sc|c)ss$/;
+const IMAGE_PATTERN = /\.(png|svg|jpg|gif)$/;
+const VENDORS_PATTERN = /[\\/]node_modules[\\/](react|react-dom)[\\/]/;
+
+const PATHS = {
+  dist: path.join(__dirname, "/dist"),
+  components: path.join(__dirname, "/js/components"),
+  config: path.join(__dirname, "/js/config"),
+  ducks: path.join(__dirname, "/js/ducks"),
+  interfaces: path.join(__dirname, "/js/interfaces"),
+  utils: path.join(__dirname, "/js/utils"),
+  css: path.join(__dirname, "/css/")
+};
+
+// Process of Templates
 const walkSync = (dir, filelist = []) => {
   fs.readdirSync(dir).forEach(file => {
     filelist = fs.statSync(path.join(dir, file)).isDirectory()
@@ -17,82 +34,90 @@ const walkSync = (dir, filelist = []) => {
   });
   return filelist;
 };
-const templatesList = walkSync('templates');
+const templatesList = walkSync("templates");
 const htmlWebpackPluginConfig = templatesList.map(file => {
   return new HtmlWebpackPlugin({
-      filename: file,
-      template: file,
-      config: appConfig,
-      inject: false,
-    });
+    filename: file,
+    template: file,
+    config: appConfig,
+    inject: false
+  });
 });
 
 const config: webpack.Configuration = {
-    entry: {
-      main: [
-        path.join(__dirname, '/css/styles.scss'),
-        path.join(__dirname, '/js/index.tsx')
-      ],
+  entry: {
+    main: [
+      path.join(__dirname, "/css/styles.scss"),
+      path.join(__dirname, "/js/index.tsx")
+    ]
+  },
+  output: {
+    publicPath: "/static/dist",
+    path: PATHS.dist,
+    filename: "[name].[contenthash].js"
+  },
+  devtool: "source-map",
+  resolve: {
+    alias: {
+      components: PATHS.components,
+      config: PATHS.config,
+      ducks: PATHS.ducks,
+      interfaces: PATHS.interfaces,
+      utils: PATHS.utils
     },
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: '[name].js',
-    },
-    devtool: 'source-map',
-    resolve: {
-        alias: {
-            components: path.join(__dirname, '/js/components'),
-            config: path.join(__dirname, '/js/config'),
-            ducks: path.join(__dirname, '/js/ducks'),
-            interfaces: path.join(__dirname, '/js/interfaces'),
-            utils: path.join(__dirname, '/js/utils'),
-        },
-        extensions: ['.tsx', '.ts', '.js', '.jsx', '.css', '.scss'],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.ts|\.tsx$/,
-          loader: 'ts-loader',
-        },
-        {
-          test: /\.jsx?$/,
-          exclude: /node_modules/,
-          use: 'babel-loader',
-        },
-        {
-          test: /\.(sa|sc|c)ss$/,
-          use: [MiniCssExtractPlugin.loader, 'css-loader', {
-            loader: 'sass-loader',
-            options: {
-              includePaths: [path.join(__dirname, '/css/')]
-            }
-          }],
-        },
-        {
-          test: /\.(png|svg|jpg|gif)$/,
-          use: 'file-loader',
-        },
-      ],
-    },
-    plugins: [
-      new CleanWebpackPlugin(),
-      new MomentLocalesPlugin(),    // To strip all locales except “en”
-      new MiniCssExtractPlugin(),
-      ...htmlWebpackPluginConfig,
-      // new BundleAnalyzerPlugin()   // Uncomment to analyze the production bundle on local
-    ],
-    optimization: {
-      splitChunks: {
-        cacheGroups: {
-          default: false,
-          major: {
-            name: 'vendors',
-            test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
-            chunks: 'all',
-          },
-        },
+    extensions: [".tsx", ".ts", ".js", ".jsx", ".css", ".scss"]
+  },
+  module: {
+    rules: [
+      {
+        test: TSX_PATTERN,
+        loader: "ts-loader"
       },
-    },
+      {
+        test: JSX_PATTERN,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      },
+      {
+        test: CSS_PATTERN,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              includePaths: [PATHS.css]
+            }
+          }
+        ]
+      },
+      {
+        test: IMAGE_PATTERN,
+        use: "file-loader"
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MomentLocalesPlugin(), // To strip all locales except “en”
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css"
+    }),
+    ...htmlWebpackPluginConfig
+    // new BundleAnalyzerPlugin()   // Uncomment to analyze the production bundle on local
+  ],
+  optimization: {
+    moduleIds: "hashed",
+    splitChunks: {
+      cacheGroups: {
+        default: false,
+        major: {
+          name: "vendors",
+          test: VENDORS_PATTERN,
+          chunks: "all"
+        }
+      }
+    }
+  }
 };
 export default config;
--- frontend/amundsen_application/static/webpack.dev.ts
+++ frontend/amundsen_application/static/webpack.dev.ts
@@ -4,6 +4,7 @@ import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer';
 import commonConfig from './webpack.common';
 
 export default merge(commonConfig, {
+  devtool: 'inline-source-map',
   plugins: [
     // new BundleAnalyzerPlugin()     // Uncomment to check the bundle size on dev
   ],
