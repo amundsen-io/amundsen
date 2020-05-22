f55346eb5b9f2f14e91cc55b4ea8be4161d0466a
Marcos Iglesias <190833+Golodhros@users.noreply.github.com>
2020-05-21 17:19:47 -0700

    Adds option to open Dashboard preview image into a modal (#460)
    * Adding tooltip and testing
    
    * Adding modal and tests
    
    * Adds hover feedback, limits modal size and cleans up tooltip code
    
    * Retrieving site.webmanifest file names and adding crossorigin attribute
    
    * Adds Tamika's comments

--- frontend/amundsen_application/static/js/components/DashboardPage/ImagePreview/constants.ts
+++ frontend/amundsen_application/static/js/components/DashboardPage/ImagePreview/constants.ts
@@ -4,4 +4,4 @@ Please view the dashboard at';
 export const PREVIEW_BASE = '/api/dashboard_preview/v0/dashboard';
 export const PREVIEW_END = 'preview.jpg';
 
-export const LOADING_TEXT = '...loading dashboard preview...'
+export const DASHBOARD_PREVIEW_MODAL_TITLE = 'Dashboard Preview'
