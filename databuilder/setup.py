307ab5456977e08fed798d906644ca22a9af09cc
Tao Feng <fengtao04@gmail.com>
2020-06-03 10:56:44 -0700

    ci: remove py2 ci (#280)

--- databuilder/setup.py
+++ databuilder/setup.py
@@ -53,7 +53,7 @@ setup(
     packages=find_packages(exclude=['tests*']),
     dependency_links=[],
     install_requires=requirements,
-    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*',
+    python_requires='>=3.6,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*',
     extras_require={
         ':python_version=="2.7"': ['typing>=3.6'],  # allow typehinting PY2
         'all': all_deps,
@@ -68,7 +68,6 @@ setup(
         'druid': druid,
     },
     classifiers=[
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
