31b8cae896c80cb7a3d6dd7882688284586575d1
Tao Feng <fengtao04@gmail.com>
2020-07-07 13:00:41 -0700

    chore: update the email for the project (#148)

--- metadata/setup.py
+++ metadata/setup.py
@@ -15,7 +15,7 @@ setup(
     description='Metadata service for Amundsen',
     url='https://www.github.com/lyft/amundsenmetadatalibrary',
     maintainer='Lyft',
-    maintainer_email='dev@lyft.com',
+    maintainer_email='amundsen-dev@lyft.com',
     packages=find_packages(exclude=['tests*']),
     include_package_data=True,
     zip_safe=False,
