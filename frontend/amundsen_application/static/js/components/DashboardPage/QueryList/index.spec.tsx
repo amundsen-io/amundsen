55d3f0df598a515d95bf71e79b3a18e3b3c75924
Tamika Tannis <ttannis@lyft.com>
2020-07-10 13:19:53 -0700

    Update index.spec.tsx

--- frontend/amundsen_application/static/js/components/DashboardPage/QueryList/index.spec.tsx
+++ frontend/amundsen_application/static/js/components/DashboardPage/QueryList/index.spec.tsx
@@ -24,7 +24,7 @@ describe('QueryList', () => {
             type: ResourceType.query,
             name: '2022-02-22 TEST QUERY NAME',
             query_text:
-              "WITH\n\ncolumnName AS (\nSELECT  split_part(columnName, 'TEST', 2) as parameter,\n        SUM(amount) as vct_spend\nFROM    hive.core.fact_passenger_spends\nWHERE   ds >= '2020-02-26'\n    AND ds < '2020-03-04'\n    AND coupon_code like '%VCT%'\n    AND coupon_code like 'USAPAXENG%'\n    AND coupon_code NOT like '%PASS%' -- EXCLUDING RIDE PASSES DUE TO 5-WEEK SPEND\n    AND coupon_code NOT like '%10CAP1X75AD%'  -- NOT SURE WHY THIS COUPON WAS BEING DEPLOYED...\n    AND coupon_code NOT like '%QAR%' -- Q1 QAR test\nGROUP BY 1\n)\n\nSELECT  offer,\n        vct_spend\nFROM    offer_spends\nUNION\nSELECT  'TOTAL' as offer,\n        SUM(vct_spend) as vct_spend\nFROM    offer_spends\nUNION   \nSELECT  'AVG_SPEND' as offer,\n        SUM(vct_spend) / COUNT(offer) * 1.0 as vct_spend\nFROM    offer_spends\nORDER BY  1",
+              "WITH\n\ncolumnName AS (\nSELECT  split_part(columnName, 'TEST', 2) as test",
             url:
               'https://app.mode.com/company/reports/testID/queries/testQuery',
           },
@@ -32,7 +32,7 @@ describe('QueryList', () => {
             type: ResourceType.query,
             name: '2022-02-23 TEST QUERY NAME TWO',
             query_text:
-              "WITH\n\ncolumnName2 AS (\nSELECT  split_part(columnName2, 'TEST', 2) as parameter,\n        SUM(amount) as vct_spend\nFROM    hive.core.fact_passenger_spends\nWHERE   ds >= '2020-02-26'\n    AND ds < '2020-03-04'\n    AND coupon_code like '%VCT%'\n    AND coupon_code like 'USAPAXENG%'\n    AND coupon_code NOT like '%PASS%' -- EXCLUDING RIDE PASSES DUE TO 5-WEEK SPEND\n    AND coupon_code NOT like '%10CAP1X75AD%'  -- NOT SURE WHY THIS COUPON WAS BEING DEPLOYED...\n    AND coupon_code NOT like '%QAR%' -- Q1 QAR test\nGROUP BY 1\n)\n\nSELECT  offer,\n        vct_spend\nFROM    offer_spends\nUNION\nSELECT  'TOTAL' as offer,\n        SUM(vct_spend) as vct_spend\nFROM    offer_spends\nUNION   \nSELECT  'AVG_SPEND' as offer,\n        SUM(vct_spend) / COUNT(offer) * 1.0 as vct_spend\nFROM    offer_spends\nORDER BY  1",
+              "WITH\n\ncolumnName AS (\nSELECT  split_part(columnName, 'TEST', 2) as test",
             url:
               'https://app.mode.com/company/reports/testID2/queries/testQuery2',
           },
