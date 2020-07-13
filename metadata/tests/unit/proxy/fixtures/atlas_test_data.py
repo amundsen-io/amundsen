a3e2f80e7e29d5ed3d74ded598f80f81d33054b7
mgorsk1 <gorskimariusz13@gmail.com>
2020-07-13 06:15:10 +0200

    feat: Add Frequent Users feature in [AtlasProxy] (#147)
    * :tada: Initial commit.
    
    * :recycle: Refactoring code.
    
    * :ok_hand: Updating code due to code review changes.
    
    * :ok_hand: Updating code due to code review changes. (tests)

--- metadata/tests/unit/proxy/fixtures/atlas_test_data.py
+++ metadata/tests/unit/proxy/fixtures/atlas_test_data.py
@@ -1,6 +1,13 @@
 import copy
 
 
+class DottedDict(dict):
+    """dot.notation access to dictionary attributes"""
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+
 class Data:
     entity_type = 'hive_table'
     column_type = 'hive_column'
@@ -166,3 +173,82 @@ class Data:
             bookmark_entity2,
         ]
     }
+
+    user_entity_1 = {
+        "typeName": "User",
+        "attributes": {
+            "qualifiedName": "test_user_1"
+        },
+        "guid": "",
+        "status": "ACTIVE",
+        "displayText": 'test_user_1',
+        "classificationNames": [],
+        "meaningNames": [],
+        "meanings": []
+    }
+
+    user_entity_2 = {
+        "typeName": "User",
+        "attributes": {
+            "qualifiedName": "test_user_2"
+        },
+        "guid": "",
+        "status": "ACTIVE",
+        "displayText": 'test_user_2',
+        "classificationNames": [],
+        "meaningNames": [],
+        "meanings": [],
+        "relationshipAttributes": {
+            "entityReads": [
+                {
+                    "entityStatus": "ACTIVE",
+                    "relationshipStatus": "ACTIVE",
+                    "guid": "1"
+                },
+                {
+                    "entityStatus": "INACTIVE",
+                    "relationshipStatus": "ACTIVE",
+                    "guid": "2"
+                },
+                {
+                    "entityStatus": "ACTIVE",
+                    "relationshipStatus": "INACTIVE",
+                    "guid": "3"
+                }
+            ]
+        }
+    }
+
+    reader_entity_1 = {
+        "typeName": "Reader",
+        "attributes": {
+            "count": 5,
+            "qualifiedName": '{}.{}.{}.reader@{}'.format(db, 'Table1', 'test_user_1', cluster),
+            "entityUri": f"hive_table://{cluster}.{db}/Table1",
+        },
+        "guid": "1",
+        "status": "ACTIVE",
+        "displayText": '{}.{}.{}.reader@{}'.format(db, 'Table1', 'test_user', cluster),
+        "classificationNames": [],
+        "meaningNames": [],
+        "meanings": [],
+        "relationshipAttributes": {"user": user_entity_1}
+    }
+
+    reader_entity_2 = {
+        "typeName": "Reader",
+        "attributes": {
+            "count": 150,
+            "qualifiedName": '{}.{}.{}.reader@{}'.format(db, 'Table1', 'test_user_2', cluster),
+            "entityUri": f"hive_table://{cluster}.{db}/Table1",
+        },
+        "guid": "2",
+        "status": "ACTIVE",
+        "displayText": '{}.{}.{}.reader@{}'.format(db, 'Table1', 'test_user_2', cluster),
+        "classificationNames": [],
+        "meaningNames": [],
+        "meanings": [],
+        "relationshipAttributes": {"user": user_entity_2}
+    }
+
+    reader_entities = [DottedDict(reader_entity) for reader_entity in [reader_entity_1, reader_entity_2]]
