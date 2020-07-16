10ba8122033fff7c811e3cedd962b602e0c200b7
Tao Feng <fengtao04@gmail.com>
2020-07-16 10:50:52 -0700

    chore: apply license headers to all the source files (#41)

--- common/setup.py
+++ common/setup.py
@@ -1,3 +1,6 @@
+# Copyright Contributors to the Amundsen project.
+# SPDX-License-Identifier: Apache-2.0
+
 from setuptools import setup, find_packages
 
 setup(
---
+++ common/tests/unit/log/__init__.py
@@ -0,0 +1,3 @@
+# Copyright Contributors to the Amundsen project.
+# SPDX-License-Identifier: Apache-2.0
+
