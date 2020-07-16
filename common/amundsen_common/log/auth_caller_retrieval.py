f8a7d42df157094663f594bef598d46b233e16d1
Tao Feng <fengtao04@gmail.com>
2020-07-16 10:50:52 -0700

    chore: apply license headers to all the source files (#41)

--- common/amundsen_common/log/auth_caller_retrieval.py
+++ common/amundsen_common/log/auth_caller_retrieval.py
@@ -1,3 +1,6 @@
+# Copyright Contributors to the Amundsen project.
+# SPDX-License-Identifier: Apache-2.0
+
 import getpass
 
 from flask import current_app as flask_app
--- common/tests/unit/log/test_action_log.py
+++ common/tests/unit/log/test_action_log.py
@@ -1,3 +1,6 @@
+# Copyright Contributors to the Amundsen project.
+# SPDX-License-Identifier: Apache-2.0
+
 import socket
 import unittest
 from contextlib import contextmanager
