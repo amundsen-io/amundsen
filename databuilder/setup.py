05ab45f4e89dfff01accd59245b03e84ac11c29a
Tao Feng <fengtao04@gmail.com>
2020-08-04 15:09:45 -0700

    chore: update deps for databuilder (#309)

--- databuilder/setup.py
+++ databuilder/setup.py
@@ -5,7 +5,7 @@ import os
 from setuptools import setup, find_packages
 
 
-__version__ = '2.6.4'
+__version__ = '2.6.5'
 
 requirements_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'requirements.txt')
 with open(requirements_path) as requirements_file:
