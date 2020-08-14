2e4a367063268a016a796cee7450892d0804872c
Marcos Iglesias <190833+Golodhros@users.noreply.github.com>
2020-08-14 12:19:37 -0700

    Organizes, themes and adds typography stories (#578)
    Signed-off-by: Marcos Iglesias <miglesiasvalle@lyft.com>

--- frontend/amundsen_application/static/.storybook/preview.ts
+++ frontend/amundsen_application/static/.storybook/preview.ts
@@ -1,1 +1,22 @@
+import anysort from 'anysort';
+import { addParameters } from '@storybook/react';
+
 import '../css/styles.scss';
+
+const categoriesOrder = [
+  'Overview/Introduction',
+  'Attributes/**',
+  'Components/**',
+];
+
+addParameters({
+  options: {
+    showRoots: true,
+    storySort: (previous, next) => {
+      const [previousStory, previousMeta] = previous;
+      const [nextStory, nextMeta] = next;
+
+      return anysort(previousMeta.kind, nextMeta.kind, categoriesOrder);
+    },
+  },
+});
---
+++ frontend/amundsen_application/static/js/stories/typography.story.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+
+export default {
+  title: 'Attributes/Typography',
+};
+
+export const Typography = () => {
+  return (
+    <>
+      <h1>Headings</h1>
+      <hr />
+      <h1>Raw h1</h1>
+      <h2>Raw h2</h2>
+      <h3>Raw h3</h3>
+      <h4>Raw h4</h4>
+      <h5>Raw h5</h5>
+      <h6>Raw h6</h6>
+      <hr />
+      <h1 className="h1">Heading with .h1</h1>
+      <h2 className="h2">Heading with .h2</h2>
+      <h3 className="h3">Heading with .h3</h3>
+      <h4 className="h4">Heading with .h4</h4>
+      <h5 className="h5">Heading with .h5</h5>
+      <h6 className="h6">Heading with .h6</h6>
+      <hr />
+      <h1 className="title-1">Heading with .title-1</h1>
+      <h1 className="title-2">Heading with .title-2</h1>
+      <h1 className="title-3">Heading with .title-3</h1>
+      <h1 className="subtitle-1">Heading with .subtitle-1</h1>
+      <h1 className="subtitle-2">Heading with .subtitle-2</h1>
+      <h1 className="subtitle-3">Heading with .subtitle-3</h1>
+    </>
+  );
+};
+
+Typography.story = {
+  name: 'Headings',
+};
+
+export const Body = () => {
+  return (
+    <>
+      <h1>Body</h1>
+      <hr />
+      <p>Raw p</p>
+      <hr />
+      <p className="body-1">Paragraph with .body-1</p>
+      <p className="body-2">Paragraph with .body-2</p>
+      <p className="body-3">Paragraph with .body-3</p>
+      <p className="body-secondary-3">Paragraph with .body-secondary-3</p>
+      <p className="body-placeholder">Paragraph with .body-placeholder</p>
+      <p className="body-link">Paragraph with .body-link</p>
+      <p className="caption">Paragraph with .caption</p>
+      <p className="column-name">Paragraph with .column-name</p>
+      <p className="resource-type">Paragraph with .resource-type</p>
+      <p className="helper-text">Paragraph with .helper-text</p>
+      <p className="text-placeholder">Paragraph with .text-placeholder</p>
+      <p className="text-secondary">Paragraph with .text-secondary</p>
+      <p className="text-primary">Paragraph with .text-primary</p>
+    </>
+  );
+};
+
+Body.story = {
+  name: 'Body Text',
+};
