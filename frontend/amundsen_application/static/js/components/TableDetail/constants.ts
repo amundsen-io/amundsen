78f16a6ac0216073970fa27922b04f611e88ccdb
Marcos Iglesias <190833+Golodhros@users.noreply.github.com>
2020-08-18 18:04:54 -0700

    Fixes error on TableDetail on Dev (#597)
    Signed-off-by: Marcos Iglesias <miglesiasvalle@lyft.com>

--- frontend/amundsen_application/static/js/components/TableDetail/constants.ts
+++ frontend/amundsen_application/static/js/components/TableDetail/constants.ts
@@ -1,3 +1,2 @@
-export const PROGRMMATIC_DESC_HEADER = 'Read-only information, auto-generated';
 export const ERROR_MESSAGE = 'Something went wrong...';
 export const EDIT_DESC_TEXT = 'Click to edit description in';
