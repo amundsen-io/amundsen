db72c146ab649e33b241bdc7b92a67092e061820
Marcos Iglesias <190833+Golodhros@users.noreply.github.com>
2020-08-28 16:00:53 -0700

    Completing storybook for Flag component (#622)
    Signed-off-by: Marcos Iglesias <miglesiasvalle@lyft.com>

--- frontend/amundsen_application/static/js/components/common/Flag/Flag.story.tsx
+++ frontend/amundsen_application/static/js/components/common/Flag/Flag.story.tsx
@@ -1,16 +1,64 @@
 import React from 'react';
+import { storiesOf } from '@storybook/react';
 
+import { BadgeStyle } from 'config/config-types';
+import StorySection from '../StorySection';
 import Flag, { CaseType } from '.';
 
-export default {
-  title: 'Components/Flags',
-  component: Flag,
-};
+const stories = storiesOf('Components/Flags', module);
 
-export const SimpleFlag = () => {
-  return <Flag caseType={CaseType.LOWER_CASE} text="Test Flag" />;
-};
-
-SimpleFlag.story = {
-  name: 'simple flag',
-};
+stories.add('Flags', () => (
+  <>
+    <StorySection title="Lower Case Flag">
+      <Flag caseType={CaseType.LOWER_CASE} text="Test Flag" />
+    </StorySection>
+    <StorySection title="Upper Case Flag">
+      <Flag caseType={CaseType.UPPER_CASE} text="Test Flag" />
+    </StorySection>
+    <StorySection title="Sentence Case Flag">
+      <Flag caseType={CaseType.SENTENCE_CASE} text="Test Flag" />
+    </StorySection>
+    <StorySection title="Default Style Flag">
+      <Flag
+        caseType={CaseType.SENTENCE_CASE}
+        text="Test Flag"
+        labelStyle={BadgeStyle.DEFAULT}
+      />
+    </StorySection>
+    <StorySection title="Primary Style Flag">
+      <Flag
+        caseType={CaseType.SENTENCE_CASE}
+        text="Test Flag"
+        labelStyle={BadgeStyle.PRIMARY}
+      />
+    </StorySection>
+    <StorySection title="Informational Style Flag">
+      <Flag
+        caseType={CaseType.SENTENCE_CASE}
+        text="Test Flag"
+        labelStyle={BadgeStyle.INFO}
+      />
+    </StorySection>
+    <StorySection title="Success Style Flag">
+      <Flag
+        caseType={CaseType.SENTENCE_CASE}
+        text="Test Flag"
+        labelStyle={BadgeStyle.SUCCESS}
+      />
+    </StorySection>
+    <StorySection title="Warning Style Flag">
+      <Flag
+        caseType={CaseType.SENTENCE_CASE}
+        text="Test Flag"
+        labelStyle={BadgeStyle.WARNING}
+      />
+    </StorySection>
+    <StorySection title="Danger Style Flag">
+      <Flag
+        caseType={CaseType.SENTENCE_CASE}
+        text="Test Flag"
+        labelStyle={BadgeStyle.DANGER}
+      />
+    </StorySection>
+  </>
+));
