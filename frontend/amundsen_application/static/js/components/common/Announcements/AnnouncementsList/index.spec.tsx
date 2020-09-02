ba0e1a3f015210cbb99ed8609fe6929c16975830
Tao Feng <fengtao04@gmail.com>
2020-09-02 10:54:16 -0700

    chore: add license check action (#638)
    * chore: add license check action
    
    Signed-off-by: feng-tao <fengtao04@gmail.com>
    
    * chore: update
    
    Signed-off-by: feng-tao <fengtao04@gmail.com>
    
    * chore: fix action
    
    Signed-off-by: feng-tao <fengtao04@gmail.com>
    
    * chore: fix files
    
    Signed-off-by: feng-tao <fengtao04@gmail.com>
    
    * chore: update
    
    Signed-off-by: feng-tao <fengtao04@gmail.com>

--- frontend/amundsen_application/static/js/components/common/Announcements/AnnouncementsList/index.spec.tsx
+++ frontend/amundsen_application/static/js/components/common/Announcements/AnnouncementsList/index.spec.tsx
@@ -1,3 +1,6 @@
+// Copyright Contributors to the Amundsen project.
+// SPDX-License-Identifier: Apache-2.0
+
 import * as React from 'react';
 import { Link, BrowserRouter } from 'react-router-dom';
 import { mount } from 'enzyme';
