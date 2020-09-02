d21b96d7aee49dc4bb79b50c4310bf67ecd874c4
Tao Feng <fengtao04@gmail.com>
2020-09-02 10:54:16 -0700

    chore: add license check action (#638)
    * chore: add license check action
    
    Signed-off-by: feng-tao <fengtao04@gmail.com>
    
    * chore: update
    
    Signed-off-by: feng-tao <fengtao04@gmail.com>
    
    * chore: fix action
    
    Signed-off-by: feng-tao <fengtao04@gmail.com>
    
    * chore: fix files
    
    Signed-off-by: feng-tao <fengtao04@gmail.com>
    
    * chore: update
    
    Signed-off-by: feng-tao <fengtao04@gmail.com>

--- frontend/amundsen_application/static/js/stories/typography.story.tsx
+++ frontend/amundsen_application/static/js/stories/typography.story.tsx
@@ -1,3 +1,6 @@
+// Copyright Contributors to the Amundsen project.
+// SPDX-License-Identifier: Apache-2.0
+
 import React, { ReactNode } from 'react';
 
 import StorySection from '../components/common/StorySection';
