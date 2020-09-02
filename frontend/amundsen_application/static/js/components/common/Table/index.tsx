8b4b4fd69e7ce7cba0ff9b5854a620f494c7e602
Marcos Iglesias <190833+Golodhros@users.noreply.github.com>
2020-09-02 10:51:12 -0700

    feat: Adds a basic Table to our component library (#636)
    Signed-off-by: Marcos Iglesias Valle <golodhros@gmail.com>

---
+++ frontend/amundsen_application/static/js/components/common/Table/index.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react';
+
+import './styles.scss';
+
+export interface TableColumn {
+  title: string;
+  field: string;
+  horAlign?: 'left' | 'right' | 'center';
+  // width?: number;
+  // className?: string;
+  // sortable?: bool (false)
+  // data?: () => React.ReactNode ((row,index) => <div>{index}</div>)
+  // actions?: Action[]
+}
+
+export interface TableProps {
+  columns: TableColumn[];
+  data: [];
+}
+
+const Table: React.FC<TableProps> = ({ data, columns }: TableProps) => {
+  const fields = columns.map(({ field }) => field);
+
+  return (
+    <table className="ams-table">
+      <thead className="ams-table-header">
+        <tr>
+          {columns.map(({ title }, index) => {
+            return (
+              <th className="ams-table-heading-cell" key={`index:${index}`}>
+                {title}
+              </th>
+            );
+          })}
+        </tr>
+      </thead>
+      <tbody className="ams-table-body">
+        {data.map((item, index) => {
+          return (
+            <tr className="ams-table-row" key={`index:${index}`}>
+              {Object.entries(item)
+                .filter(([key]) => fields.includes(key))
+                .map(([, value], index) => (
+                  <td className="ams-table-cell" key={`index:${index}`}>
+                    {value}
+                  </td>
+                ))}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
