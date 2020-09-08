61e28418958d47f679d2387740d5a147b148d5f5
Allison Suarez Miranda <22477579+allisonsuarez@users.noreply.github.com>
2020-09-08 10:31:51 -0700

    feat: add search header (#643)
    * added search header, unsure if this is the right place for this component
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * lint and small fixes
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * updated text
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * got alignmed right
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * lint :(
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>

---
+++ frontend/amundsen_application/static/js/components/common/ResourceList/ResourceListHeader/index.tsx
@@ -0,0 +1,28 @@
+// Copyright Contributors to the Amundsen project.
+// SPDX-License-Identifier: Apache-2.0
+
+import * as React from 'react';
+
+import './styles.scss';
+
+import {
+  DATASET_HEADER_TITLE,
+  SOURCE_HEADER_TITLE,
+  BADGES_HEADER_TITLE,
+} from './constants';
+
+const ResourceListHeader: React.FC = () => {
+  return (
+    <div className="resource-list-header">
+      <span className="dataset">
+        <span className="dataset-text">{DATASET_HEADER_TITLE}</span>
+      </span>
+      <span className="source">{SOURCE_HEADER_TITLE}</span>
+      <span className="badges">
+        <span className="badges-text">{BADGES_HEADER_TITLE}</span>
+      </span>
+    </div>
+  );
+};
+
+export default ResourceListHeader;
