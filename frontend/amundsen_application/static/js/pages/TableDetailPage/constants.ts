827fe3519b0c26159518e2d965373295e6c7aa05
Tamika Tannis <ttannis@lyft.com>
2020-09-09 13:22:50 -0700

    chore: Update order of table metadata on TableDetailPage (#652)
    * Update metadata order
    
    Signed-off-by: Tamika Tannis <ttannis@lyft.com>
    
    * Code cleanup
    
    Signed-off-by: Tamika Tannis <ttannis@lyft.com>
    
    * Lint fix
    
    Signed-off-by: Tamika Tannis <ttannis@lyft.com>
    
    * Betterer update
    
    Signed-off-by: Tamika Tannis <ttannis@lyft.com>

--- frontend/amundsen_application/static/js/pages/TableDetailPage/constants.ts
+++ frontend/amundsen_application/static/js/pages/TableDetailPage/constants.ts
@@ -1,2 +1,9 @@
 export const ERROR_MESSAGE = 'Something went wrong...';
 export const EDIT_DESC_TEXT = 'Click to edit description in';
+
+export const DATE_RANGE_TITLE = 'Date Range';
+export const DESCRIPTION_TITLE = 'Description';
+export const FREQ_USERS_TITLE = 'Frequent Users';
+export const LAST_UPDATED_TITLE = 'Last Updated';
+export const OWNERS_TITLE = 'Owners';
+export const TAG_TITLE = 'Tags';
