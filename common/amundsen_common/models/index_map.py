8f80fd742aa5e45724cbb2ddd86b2f98a231cb20
Wonyeong Choi <ciwnyg0815@gmail.com>
2020-09-25 13:23:03 +0900

    fix: Add dashboard index map copied from amundsendatabuilder (#65)
    Signed-off-by: Wonyeong Choi <ciwnyg0815@gmail.com>

--- common/amundsen_common/models/index_map.py
+++ common/amundsen_common/models/index_map.py
@@ -98,6 +98,83 @@ TABLE_INDEX_MAP = textwrap.dedent(
     """
 )
 
+DASHBOARD_ELASTICSEARCH_INDEX_MAPPING = textwrap.dedent(
+    """
+    {
+        "settings": {
+          "analysis": {
+            "normalizer": {
+              "lowercase_normalizer": {
+                "type": "custom",
+                "char_filter": [],
+                "filter": ["lowercase", "asciifolding"]
+              }
+            }
+          }
+        },
+        "mappings":{
+            "dashboard":{
+              "properties": {
+                "group_name": {
+                  "type":"text",
+                  "analyzer": "simple",
+                  "fields": {
+                    "raw": {
+                      "type": "keyword",
+                      "normalizer": "lowercase_normalizer"
+                    }
+                  }
+                },
+                "name": {
+                  "type":"text",
+                  "analyzer": "simple",
+                  "fields": {
+                    "raw": {
+                      "type": "keyword",
+                      "normalizer": "lowercase_normalizer"
+                    }
+                  }
+                },
+                "description": {
+                  "type":"text",
+                  "analyzer": "simple",
+                  "fields": {
+                    "raw": {
+                      "type": "keyword"
+                    }
+                  }
+                },
+                "group_description": {
+                  "type":"text",
+                  "analyzer": "simple",
+                  "fields": {
+                    "raw": {
+                      "type": "keyword"
+                    }
+                  }
+                },
+                "query_names": {
+                  "type":"text",
+                  "analyzer": "simple",
+                  "fields": {
+                    "raw": {
+                      "type": "keyword"
+                    }
+                  }
+                },
+                "tags": {
+                  "type": "keyword"
+                },
+                "badges": {
+                  "type": "keyword"
+                }
+              }
+            }
+          }
+        }
+    """
+)
+
 USER_INDEX_MAP = textwrap.dedent("""
 {
 "mappings":{
