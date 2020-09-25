747d7f8fa7b52dda7d7fbb676ec1bbd97aa2c0e7
Wonyeong Choi <ciwnyg0815@gmail.com>
2020-09-25 13:24:47 +0900

    feat: Add marshmallow packages to setup.py (#66)
    Signed-off-by: Wonyeong Choi <ciwnyg0815@gmail.com>

--- common/setup.py
+++ common/setup.py
@@ -31,6 +31,8 @@ setup(
         # long as they have a version of pyfoobar equal to or greater than 1.x
         # and less than 2.x installed.
         'flask>=1.0.2',
+        'marshmallow>=2.15.3'
+        'marshmallow-annotations>=2.4.0'
     ],
     python_requires=">=3.6",
     package_data={'amundsen_common': ['py.typed']},
