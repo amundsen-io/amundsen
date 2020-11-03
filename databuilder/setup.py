4e9462967756663482b79ae42cff3254d59f68fe
Hugo Hobson <hugo.hobson@dazn.com>
2020-11-03 15:33:32 +0000

    fix: Add databuilder missing dependencies (#400)
    * Add dependencies missing from version 4.0.2
    
    Signed-off-by: hhobson <hugo.hobson@dazn.com>
    
    * Update setup.py
    
    Signed-off-by: hhobson <hugo.hobson@dazn.com>

--- databuilder/setup.py
+++ databuilder/setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 
-__version__ = '4.0.2'
+__version__ = '4.0.3'
 
 
 requirements = [
@@ -16,6 +16,8 @@ requirements = [
     "elasticsearch>=6.2.0,<7.0",
     "pyhocon>=0.3.42",
     "unidecode",
+    "Jinja2>=2.10.0,<2.12",
+    "pandas>=0.21.0,<1.2.0"
 ]
 
 kafka = ['confluent-kafka==1.0.0']
