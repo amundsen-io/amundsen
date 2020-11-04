23ac9c1c0e521d02b817ac5345e78bded35b9b41
Allison Suarez Miranda <22477579+allisonsuarez@users.noreply.github.com>
2020-11-04 13:19:39 -0800

    chore: updated version of library for new release (#225)
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>

--- metadata/setup.py
+++ metadata/setup.py
@@ -5,7 +5,7 @@ import os
 
 from setuptools import setup, find_packages
 
-__version__ = '2.5.5'
+__version__ = '3.0.0'
 
 
 requirements_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'requirements.txt')
