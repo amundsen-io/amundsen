c163dc5d02fb274eace70c9ac149c4e57e438d14
Allison Suarez Miranda <22477579+allisonsuarez@users.noreply.github.com>
2020-11-24 16:31:46 -0800

    fix: fixed webpack 5 issue (#786)
    * added plugin to fix webpack issue
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * install process
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>

--- frontend/amundsen_application/static/webpack.dev.ts
+++ frontend/amundsen_application/static/webpack.dev.ts
@@ -4,12 +4,18 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 import commonConfig from './webpack.common';
 
+const webpack = require('webpack');
+
 export default merge(commonConfig, {
   mode: 'development',
   devtool: 'inline-source-map',
   plugins: [
     new MiniCssExtractPlugin({
       filename: '[name].dev.css',
+    }),
+    // fix "process is not defined" error:
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
     }),
     // new BundleAnalyzerPlugin()     // Uncomment to check the bundle size on dev
   ],
