2e4be27b8eae9f614bb17a98865a06b29b8adbd4
Aarni Alasaarela <aarni.alasaarela@gmail.com>
2020-11-25 19:10:22 +0200

    chore: Update request_utils.py (#787)

--- frontend/amundsen_application/api/utils/request_utils.py
+++ frontend/amundsen_application/api/utils/request_utils.py
@@ -10,7 +10,7 @@ from flask import current_app as app
 def get_query_param(args: Dict, param: str, error_msg: str = None) -> str:
     value = args.get(param)
     if value is None:
-        msg = 'A {0} parameter must be provided'.format(param) if error_msg is not None else error_msg
+        msg = 'A {0} parameter must be provided'.format(param) if error_msg is None else error_msg
         raise Exception(msg)
     return value
 
