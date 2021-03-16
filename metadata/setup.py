04a9c426f0e3c9b9cd69d0c818d1a133fad4dbab
Verdan Mahmood <verdan.mahmood@gmail.com>
2021-03-16 05:54:46 +0100

    makes sure to install all requirements (#281)
    Signed-off-by: verdan <verdan.mahmood@gmail.com>

--- metadata/setup.py
+++ metadata/setup.py
@@ -22,14 +22,7 @@ setup(
     packages=find_packages(exclude=['tests*']),
     include_package_data=True,
     zip_safe=False,
-    dependency_links=[
-        ('git+https://www.github.com/hilearn/marshmallow-'
-         'annotations.git@a7a2dc96932430369bdef36555082df990ed9bef#egg=marshmallow-annotations')
-    ],
-    install_requires=[
-        'marshmallow>=3.0,<=3.6',
-        'marshmallow-annotations'
-    ],
+    install_requires=requirements,
     extras_require={
         'oidc': ['flaskoidc==0.1.1']
     },
