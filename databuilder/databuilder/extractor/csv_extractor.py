ccb458553a02415394b8eaf5a4e8781ad5199c52
sewardgw <Sewardgw@gmail.com>
2021-04-10 18:13:51 -0400

    feat: Allow multiple badges on a single record for csv extracts (#469)
    * Allow multiple badges on a single record for csv extracts
    
    Signed-off-by: Grant Seward <grant@stemma.ai>
    
    * Fixed make errors
    
    Signed-off-by: Grant Seward <grant@stemma.ai>
    
    * Separator spelling
    
    Signed-off-by: Grant Seward <grant@stemma.ai>

--- databuilder/databuilder/extractor/csv_extractor.py
+++ databuilder/databuilder/extractor/csv_extractor.py
@@ -4,13 +4,23 @@
 import csv
 import importlib
 from collections import defaultdict
-from typing import Any
+from typing import Any, List
 
 from pyhocon import ConfigTree
 
 from databuilder.extractor.base_extractor import Extractor
 from databuilder.models.badge import Badge, BadgeMetadata
 from databuilder.models.table_metadata import ColumnMetadata, TableMetadata
+
+
+def split_badge_list(badges: str, separator: str) -> List[str]:
+    """
+    Splits a string of badges into a list, removing all empty badges.
+    """
+    if badges is None:
+        return []
+
+    return [badge for badge in badges.split(separator) if badge]
 
 
 class CsvExtractor(Extractor):
@@ -70,6 +80,7 @@ class CsvTableBadgeExtractor(Extractor):
     # Config keys
     TABLE_FILE_LOCATION = 'table_file_location'
     BADGE_FILE_LOCATION = 'badge_file_location'
+    BADGE_SEPARATOR = 'badge_separator'
 
     """
     An Extractor that combines Table and Badge CSVs.
@@ -78,6 +89,7 @@ class CsvTableBadgeExtractor(Extractor):
         self.conf = conf
         self.table_file_location = conf.get_string(CsvTableBadgeExtractor.TABLE_FILE_LOCATION)
         self.badge_file_location = conf.get_string(CsvTableBadgeExtractor.BADGE_FILE_LOCATION)
+        self.badge_separator = conf.get_string(CsvTableBadgeExtractor.BADGE_SEPARATOR, default=',')
         self._load_csv()
 
     def _get_key(self,
@@ -103,9 +115,11 @@ class CsvTableBadgeExtractor(Extractor):
             schema = badge_dict['schema']
             table_name = badge_dict['table_name']
             id = self._get_key(db, cluster, schema, table_name)
-            badge = Badge(name=badge_dict['name'],
-                          category=badge_dict['category'])
-            parsed_badges[id].append(badge)
+            split_badges = split_badge_list(badges=badge_dict['name'],
+                                            separator=self.badge_separator)
+            for badge_name in split_badges:
+                badge = Badge(name=badge_name, category=badge_dict['category'])
+                parsed_badges[id].append(badge)
 
         with open(self.table_file_location, 'r') as fin:
             tables = [dict(i) for i in csv.DictReader(fin)]
@@ -147,6 +161,7 @@ class CsvTableColumnExtractor(Extractor):
     # Config keys
     TABLE_FILE_LOCATION = 'table_file_location'
     COLUMN_FILE_LOCATION = 'column_file_location'
+    BADGE_SEPARATOR = 'badge_separator'
 
     """
     An Extractor that combines Table and Column CSVs.
@@ -159,6 +174,7 @@ class CsvTableColumnExtractor(Extractor):
         self.conf = conf
         self.table_file_location = conf.get_string(CsvTableColumnExtractor.TABLE_FILE_LOCATION)
         self.column_file_location = conf.get_string(CsvTableColumnExtractor.COLUMN_FILE_LOCATION)
+        self.badge_separator = conf.get_string(CsvTableBadgeExtractor.BADGE_SEPARATOR, default=',')
         self._load_csv()
 
     def _get_key(self,
@@ -187,12 +203,14 @@ class CsvTableColumnExtractor(Extractor):
             schema = column_dict['schema']
             table_name = column_dict['table_name']
             id = self._get_key(db, cluster, schema, table_name)
+            split_badges = split_badge_list(badges=column_dict['badges'],
+                                            separator=self.badge_separator)
             column = ColumnMetadata(
                 name=column_dict['name'],
                 description=column_dict['description'],
                 col_type=column_dict['col_type'],
                 sort_order=int(column_dict['sort_order']),
-                badges=[column_dict['badges']]
+                badges=split_badges
             )
             parsed_columns[id].append(column)
 
--- databuilder/tests/unit/extractor/test_csv_extractor.py
+++ databuilder/tests/unit/extractor/test_csv_extractor.py
@@ -6,7 +6,9 @@ import unittest
 from pyhocon import ConfigFactory
 
 from databuilder import Scoped
-from databuilder.extractor.csv_extractor import CsvExtractor, CsvTableColumnExtractor
+from databuilder.extractor.csv_extractor import (
+    CsvExtractor, CsvTableBadgeExtractor, CsvTableColumnExtractor, split_badge_list,
+)
 from databuilder.models.badge import Badge
 
 
@@ -42,6 +44,26 @@ class TestCsvExtractor(unittest.TestCase):
         self.assertEqual(result3.name, 'test_view1')
         self.assertEqual(result3.is_view, 'true')
 
+    def test_extraction_table_badges(self) -> None:
+        """
+        Tests that badges are properly parsed from a CSV file and assigned to a table.
+        """
+        config_dict = {
+            f'extractor.csvtablebadge.{CsvTableBadgeExtractor.TABLE_FILE_LOCATION}':
+            'example/sample_data/sample_table.csv',
+            f'extractor.csvtablebadge.{CsvTableBadgeExtractor.BADGE_FILE_LOCATION}':
+            'example/sample_data/sample_badges.csv',
+        }
+        self.conf = ConfigFactory.from_dict(config_dict)
+        extractor = CsvTableBadgeExtractor()
+        extractor.init(Scoped.get_scoped_conf(conf=self.conf,
+                                              scope=extractor.get_scope()))
+        result_1 = extractor.extract()
+        self.assertEqual([b.name for b in result_1.badges], ['beta'])
+
+        result_2 = extractor.extract()
+        self.assertEqual([b.name for b in result_2.badges], ['json', 'npi'])
+
     def test_extraction_of_tablecolumn_badges(self) -> None:
         """
         Test Extraction using the combined CsvTableModel model class
@@ -62,3 +84,20 @@ class TestCsvExtractor(unittest.TestCase):
         self.assertEqual(result.name, 'test_table1')
         self.assertEqual(result.columns[0].badges, [Badge('pk', 'column')])
         self.assertEqual(result.columns[1].badges, [Badge('pii', 'column')])
+        self.assertEqual(result.columns[2].badges, [Badge('fk', 'column'), Badge('pii', 'column')])
+
+    def test_split_badge_list(self) -> None:
+        """
+        Test spliting a string of badges into a list, removing all empty badges.
+        """
+        badge_list_1 = 'badge1'
+        result_1 = split_badge_list(badges=badge_list_1, separator=',')
+        self.assertEqual(result_1, ['badge1'])
+
+        badge_list_2 = ''
+        result_2 = split_badge_list(badges=badge_list_2, separator=',')
+        self.assertEqual(result_2, [])
+
+        badge_list_3 = 'badge1|badge2|badge3'
+        result_3 = split_badge_list(badges=badge_list_3, separator='|')
+        self.assertEqual(result_3, ['badge1', 'badge2', 'badge3'])
