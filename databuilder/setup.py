12f3dfb68cfea8f31c02bb83e25a1c5d5d12f855
Fokko Driesprong <fokko@datafold.com>
2021-05-18 21:35:25 +0200

    test: Add Python 3.8 and 3.9 databuilder CI
    Ran the databuilder locally using 3.9 and it works fine, would be
    a pity to not add this as a supported version
    
    Signed-off-by: Fokko Driesprong <fokko@datafold.com>

--- databuilder/setup.py
+++ databuilder/setup.py
@@ -120,5 +120,7 @@ setup(
     classifiers=[
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 )
