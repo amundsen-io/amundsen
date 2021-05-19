a173c9e7b44c8212a32fcbcfb4411c071b04668f
Daniel Won <dwon@lyft.com>
2021-05-19 11:15:38 -0700

    Removed merge conflict error
    Signed-off-by: Daniel Won <dwon@lyft.com>

--- frontend/amundsen_application/static/js/ducks/tableMetadata/api/v0.ts
+++ frontend/amundsen_application/static/js/ducks/tableMetadata/api/v0.ts
@@ -197,43 +197,6 @@ export function getPreviewData(queryParams: TablePreviewQueryParams) {
     });
 }
 
-export function getTableLineage(key: string) {
-  const tableQueryParams = getTableQueryParams({ key });
-  return axios({
-    url: `${API_PATH}/get_table_lineage?${tableQueryParams}`,
-    method: 'GET',
-  })
-    .then((response: AxiosResponse<LineageAPI>) => ({
-      data: response.data,
-      status: response.status,
-    }))
-    .catch((e: AxiosError<LineageAPI>) => {
-      const { response } = e;
-      const status = response ? response.status : null;
-      return Promise.reject({ status });
-    });
-}
-
-export function getColumnLineage(key: string, columnName: string) {
-  const tableQueryParams = getTableQueryParams({
-    key,
-    column_name: columnName,
-  });
-  return axios({
-    url: `${API_PATH}/get_column_lineage?${tableQueryParams}`,
-    method: 'GET',
-  })
-    .then((response: AxiosResponse<LineageAPI>) => ({
-      data: response.data,
-      status: response.status,
-    }))
-    .catch((e: AxiosError<LineageAPI>) => {
-      const { response } = e;
-      const status = response ? response.status : null;
-      return Promise.reject({ status });
-    });
-}
-
 export function getTableQualityChecks(key: string) {
   const tableQueryParams = getTableQueryParams({
     key,
