ed1ce55b31edc844bcf90b3c5190b25734367bba
Allison Suarez Miranda <asuarezmiranda@lyft.com>
2021-05-24 09:47:36 -0700

    correct feat key
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>

--- frontend/tests/unit/api/metadata/test_v0.py
+++ frontend/tests/unit/api/metadata/test_v0.py
@@ -1134,7 +1134,7 @@ class MetadataTest(unittest.TestCase):
             response = test.get(
                 '/api/metadata/v0/feature',
                 query_string=dict(
-                    key='db://cluster.schema/feature',
+                    key='/test_feature_group/test_feature_name/1.4',
                     index='0',
                     source='test_source'
                 )
