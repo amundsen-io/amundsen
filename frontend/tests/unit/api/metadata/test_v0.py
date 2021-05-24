06d0117c3c2ed2d422b7a71f7972fc1c9a9bd02d
Allison Suarez Miranda <asuarezmiranda@lyft.com>
2021-05-24 13:46:21 -0700

    expected metadata
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>

--- frontend/tests/unit/api/metadata/test_v0.py
+++ frontend/tests/unit/api/metadata/test_v0.py
@@ -360,6 +360,40 @@ class MetadataTest(unittest.TestCase):
             ],
             'badges': [{'category': 'data', 'badge_name': 'pii'}],
         }
+        self.expected_feature_metadata = {
+            'partition_column': {
+                'name': 'ds',
+                'description': 'This is a test',
+                'col_type': 'bigint',
+                'sort_order': 0,
+                'badges': [
+                    {'category': 'column', 'badge_name': 'partition_column'}
+                ]
+            },
+            'entity': 'rider',
+            'key': 'test_feature_group/test_feature_name/1.4',
+            'availability': ['hive'],
+            'last_updated_timestamp': 1563872712,
+            'owners': [],
+            'name': 'test_feature_name',
+            'description': 'This is a test',
+            'status': None,
+            'programmatic_descriptions': [
+                {'source': 'c_1', 'text': 'description c'},
+                {'source': 'a_1', 'text': 'description a'},
+                {'source': 'b_1', 'text': 'description b'}
+            ],
+            'owner_tags': [],
+            'data_type': 'bigint',
+            'feature_group': 'test_feature_group',
+            'version': '1.4',
+            'tags': [],
+            'watermarks': [
+                {'watermark_type': 'low_watermark', 'partition_key': 'ds', 'partition_value': '', 'create_time': ''},
+                {'watermark_type': 'high_watermark', 'partition_key': 'ds', 'partition_value': '', 'create_time': ''}
+            ],
+            'badges': [{'category': 'data', 'badge_name': 'pii'}],
+        }
         self.mock_dashboard_metadata = {
             "badges": [],
             "chart_names": [],
@@ -1173,4 +1207,4 @@ class MetadataTest(unittest.TestCase):
             )
             data = json.loads(response.data)
             self.assertEqual(response.status_code, HTTPStatus.OK)
-            self.assertCountEqual(data.get('featureData'), self.expected_parsed_metadata)
+            self.assertCountEqual(data.get('featureData'), self.expected_feature_metadata)
