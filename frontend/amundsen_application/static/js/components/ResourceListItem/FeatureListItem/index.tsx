d9a39f95800ab79be9977ce9bbc12961d3b4de27
Allison Suarez Miranda <asuarezmiranda@lyft.com>
2021-06-10 10:58:39 -0700

    refactor to functional component
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>

--- frontend/amundsen_application/static/js/components/ResourceListItem/FeatureListItem/index.tsx
+++ frontend/amundsen_application/static/js/components/ResourceListItem/FeatureListItem/index.tsx
@@ -17,68 +17,66 @@ export interface FeatureListItemProps {
   logging: LoggingParams;
 }
 
-class FeatureListItem extends React.Component<FeatureListItemProps, {}> {
-  getLink = () => {
-    const { feature, logging } = this.props;
+const getLink = (feature: FeatureResource, logging: LoggingParams) => {
+  return `/feature/${feature.key}?index=${logging.index}&source=${logging.source}`;
+};
 
-    return `/feature/${feature.key}?index=${logging.index}&source=${logging.source}`;
-  };
+const generateResourceIconClass = (
+  featureId: string,
+  featureType: ResourceType
+): string =>
+  `icon resource-icon ${getSourceIconClass(featureId, featureType)}`;
 
-  generateResourceIconClass = (
-    featureId: string,
-    featureType: ResourceType
-  ): string =>
-    `icon resource-icon ${getSourceIconClass(featureId, featureType)}`;
-
-  render() {
-    const { feature, logging } = this.props;
-    const source =
-      feature.availability.length > 0 ? feature.availability[0] : '';
-    return (
-      <li className="list-group-item clickable">
-        <Link
-          className="resource-list-item table-list-item"
-          to={this.getLink()}
-          onClick={(e) =>
-            logClick(e, {
-              target_id: 'feature_list_item',
-              value: logging.source,
-            })
-          }
-        >
-          <div className="resource-info">
-            <span
-              className={this.generateResourceIconClass(source, feature.type)}
-            />
-            <div className="resource-info-text my-auto">
-              <div className="resource-name">
-                {`${feature.feature_group}.${feature.name}`}
-              </div>
-              <div className="body-secondary-3 truncated">
-                {feature.description}
-              </div>
+const FeatureListItem: React.FC<FeatureListItemProps> = ({
+  feature,
+  logging
+}: FeatureListItemProps) => {
+  const source =
+    feature.availability.length > 0 ? feature.availability[0] : '';
+  return (
+    <li className="list-group-item clickable">
+      <Link
+        className="resource-list-item table-list-item"
+        to={getLink(feature, logging)}
+        onClick={(e) =>
+          logClick(e, {
+            target_id: 'feature_list_item',
+            value: logging.source,
+          })
+        }
+      >
+        <div className="resource-info">
+          <span
+            className={generateResourceIconClass(source, feature.type)}
+          />
+          <div className="resource-info-text my-auto">
+            <div className="resource-name">
+              {`${feature.feature_group}.${feature.name}`}
+            </div>
+            <div className="body-secondary-3 truncated">
+              {feature.description}
             </div>
           </div>
-          <div className="resource-type">
-            {getSourceDisplayName(source, feature.type)}
-          </div>
-          <div className="resource-badges">
-            {!!feature.badges && feature.badges.length > 0 && (
-              <div>
-                <div className="body-secondary-3">
-                  <BadgeList badges={feature.badges} />
-                </div>
+        </div>
+        <div className="resource-type">
+          {getSourceDisplayName(source, feature.type)}
+        </div>
+        <div className="resource-badges">
+          {!!feature.badges && feature.badges.length > 0 && (
+            <div>
+              <div className="body-secondary-3">
+                <BadgeList badges={feature.badges} />
               </div>
-            )}
-          </div>
-          <div className="resource-entity">
-            <div>{feature.entity}</div>
-            <img className="icon icon-right" alt="" />
-          </div>
-        </Link>
-      </li>
-    );
-  }
-}
+            </div>
+          )}
+        </div>
+        <div className="resource-entity">
+          <div>{feature.entity}</div>
+          <img className="icon icon-right" alt="" />
+        </div>
+      </Link>
+    </li>
+  );
+};
 
 export default FeatureListItem;
