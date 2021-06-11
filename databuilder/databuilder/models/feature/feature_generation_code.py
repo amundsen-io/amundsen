daf8a2adebabe88b5dfe114311bfbfd10fba3472
Dmitriy Kunitskiy <dkunitskiy@lyft.com>
2021-06-11 14:52:38 -0700

    linter
    Signed-off-by: Dmitriy Kunitskiy <dkunitskiy@lyft.com>

--- databuilder/databuilder/models/feature/feature_generation_code.py
+++ databuilder/databuilder/models/feature/feature_generation_code.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from typing import (
-    Any, Iterator, Optional, Union, Dict,
+    Any, Dict, Iterator, Optional, Union,
 )
 
 from databuilder.models.feature.feature_metadata import FeatureMetadata
