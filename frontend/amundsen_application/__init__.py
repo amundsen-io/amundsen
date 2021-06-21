a0d627b1643515f12c3fe1ac43bcc890a4bc924d
Verdan Mahmood <verdan.mahmood@gmail.com>
2021-06-21 13:20:34 +0200

    Makes sure to use the new env variables
    Signed-off-by: verdan <verdan.mahmood@gmail.com>

--- frontend/amundsen_application/__init__.py
+++ frontend/amundsen_application/__init__.py
@@ -6,6 +6,7 @@ import importlib
 import logging
 import logging.config
 import os
+import sys
 import warnings
 
 from flask import Flask, Blueprint
@@ -40,6 +41,9 @@ if os.getenv('APP_WRAPPER_ARGS'):
 
 """ Support for importing a subclass of flask.Flask, via env variables """
 if FLASK_APP_MODULE_NAME and FLASK_APP_CLASS_NAME:
+    if FLASK_APP_MODULE_NAME and FLASK_APP_CLASS_NAME:
+        print('Using requested Flask module {module_name} and class {class_name}'
+              .format(module_name=FLASK_APP_MODULE_NAME, class_name=FLASK_APP_CLASS_NAME), file=sys.stderr)
     moduleName = os.getenv('APP_WRAPPER', '')
     module = importlib.import_module(moduleName)
     moduleClass = os.getenv('APP_WRAPPER_CLASS', '')
