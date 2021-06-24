6ad67b5ef2b76414d2b33deacb860e66456beb3c
Dmitriy Kunitskiy <dkunitskiy@lyft.com>
2021-06-24 13:21:17 -0700

    refactor: undo the great unpinning (#1267)
    * undo the great unpinning
    
    Signed-off-by: Dmitriy Kunitskiy <dkunitskiy@lyft.com>
    
    * remove all extra requires
    
    Signed-off-by: Dmitriy Kunitskiy <dkunitskiy@lyft.com>
    
    * dev back to all
    
    Signed-off-by: Dmitriy Kunitskiy <dkunitskiy@lyft.com>
    Signed-off-by: Elena Boal <elenaboal@lyft.com>

--- common/setup.py
+++ common/setup.py
@@ -4,20 +4,13 @@ import os
 
 from setuptools import find_packages, setup
 
-__version__ = '0.16.0'
+__version__ = '0.17.0'
 
-requirements_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'requirements-common.txt')
-with open(requirements_path) as requirements_file:
-    requirements_common = requirements_file.readlines()
 
 requirements_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'requirements-dev.txt')
 with open(requirements_path) as requirements_file:
     requirements_dev = requirements_file.readlines()
 
-# avoid circular references
-requirements_common = [r for r in requirements_common if not r.startswith('amundsen-common')]
-
-all_deps = requirements_common + requirements_dev
 
 setup(
     name='amundsen-common',
@@ -29,10 +22,29 @@ setup(
     maintainer='Amundsen TSC',
     maintainer_email='amundsen-tsc@lists.lfai.foundation',
     packages=find_packages(exclude=['tests*']),
-    install_requires=requirements_common,
+    install_requires=[
+        # Packages in here should rarely be pinned. This is because these
+        # packages (at the specified version) are required for project
+        # consuming this library. By pinning to a specific version you are the
+        # number of projects that can consume this or forcing them to
+        # upgrade/downgrade any dependencies pinned here in their project.
+        #
+        # Generally packages listed here are pinned to a major version range.
+        #
+        # e.g.
+        # Python FooBar package for foobaring
+        # pyfoobar>=1.0, <2.0
+        #
+        # This will allow for any consuming projects to use this library as
+        # long as they have a version of pyfoobar equal to or greater than 1.x
+        # and less than 2.x installed.
+        'Flask>=1.0.2',
+        'attrs>=19.0.0',
+        'marshmallow>=3.0',
+        'marshmallow3-annotations>=1.0.0'
+    ],
     extras_require={
-        'all': all_deps,
-        'dev': requirements_dev
+        'all': requirements_dev
     },
     python_requires=">=3.6",
     package_data={'amundsen_common': ['py.typed']},
