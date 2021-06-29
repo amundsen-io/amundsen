a18fd78e2aba43233fb9e69e4842238f8a648fa2
dechoma <dominik.choma@gmail.com>
2021-06-29 09:48:04 +0200

    feat: intorduce atlas dashboard bookmarks (#1208)
    Signed-off-by: dechoma <dominik.choma@gmail.com>

--- metadata/setup.py
+++ metadata/setup.py
@@ -20,8 +20,7 @@ with open(requirements_path) as requirements_file:
     requirements_dev = requirements_file.readlines()
 
 oidc = ['flaskoidc==1.0.0']
-atlas = ['apache-atlas==0.0.11',
-         'amundsenatlastypes==1.2.2']
+atlas = ['apache-atlas==0.0.11']
 rds = ['amundsen-rds==0.0.5',
        'mysqlclient>=1.3.6,<3',
        'sqlalchemy>=1.3.6,<1.4',
