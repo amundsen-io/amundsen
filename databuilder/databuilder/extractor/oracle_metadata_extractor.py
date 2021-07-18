2396115d3095fa946609f15059d088719b2df3c3
Savio <savioteles@gmail.com>
2021-07-18 11:56:33 -0300

    Added Oracle extractor loader to Amundsen, including unit tests.
    Signed-off-by: Savio <savioteles@gmail.com>

---
+++ databuilder/databuilder/extractor/oracle_metadata_extractor.py
@@ -0,0 +1,48 @@
+# Copyright Contributors to the Amundsen project.
+# SPDX-License-Identifier: Apache-2.0
+
+from typing import (  # noqa: F401
+    Any, Dict, Iterator, Union,
+)
+
+from pyhocon import ConfigFactory, ConfigTree  # noqa: F401
+
+from databuilder.extractor.base_oracle_metadata_extractor import BaseOracleMetadataExtractor
+
+
+class OracleMetadataExtractor(BaseOracleMetadataExtractor):
+    """
+    Extracts Oracle table and column metadata from underlying meta store database using SQLAlchemyExtractor
+    """
+
+    def get_sql_statement(self, use_catalog_as_cluster_name: bool, where_clause_suffix: str) -> str:
+        if use_catalog_as_cluster_name:
+            cluster_source = "c.table_catalog"
+        else:
+            cluster_source = f"'{self._cluster}'"
+
+        return """
+        SELECT 
+            {cluster_source} as "cluster",
+            lower(c.OWNER) as "schema",
+            lower(c.TABLE_NAME) as "name",
+            tc.COMMENTS as "description",
+            lower(c.column_name) as "col_name",
+            lower(c.DATA_TYPE) as "col_type",
+            cc.comments as "col_description",
+            lower(c.column_id) as "col_sort_order"
+        FROM
+            all_tab_columns c
+        LEFT JOIN
+            ALL_TAB_COMMENTS tc ON c.OWNER=tc.owner AND c.TABLE_NAME=tc.TABLE_NAME
+        LEFT JOIN
+            ALL_col_comments cc ON c.owner=cc.owner AND c.TABLE_NAME=cc.TABLE_NAME AND c.column_name=cc.column_name
+        {where_clause_suffix}
+        ORDER BY "cluster", "schema", "name", "col_sort_order" ;
+        """.format(
+            cluster_source=cluster_source,
+            where_clause_suffix=where_clause_suffix,
+        )
+
+    def get_scope(self) -> str:
+        return 'extractor.oracle_metadata'
