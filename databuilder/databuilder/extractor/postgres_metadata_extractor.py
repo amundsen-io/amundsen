6cf8965ce8b065cc68be8fe021b1e7496b4b7716
Verdan Mahmood <verdan.mahmood@gmail.com>
2021-07-21 20:26:49 +0200

    refactor: Updates Postgres extractor to not use information_schema (#1365)
    * updates pg extractor to not use information_schema
    
    Signed-off-by: verdan <verdan.mahmood@gmail.com>
    
    * Fixes the test case
    
    Signed-off-by: verdan <verdan.mahmood@gmail.com>
    
    * fixes the lint issue
    
    Signed-off-by: verdan <verdan.mahmood@gmail.com>

--- databuilder/databuilder/extractor/postgres_metadata_extractor.py
+++ databuilder/databuilder/extractor/postgres_metadata_extractor.py
@@ -17,24 +17,35 @@ class PostgresMetadataExtractor(BasePostgresMetadataExtractor):
 
     def get_sql_statement(self, use_catalog_as_cluster_name: bool, where_clause_suffix: str) -> str:
         if use_catalog_as_cluster_name:
-            cluster_source = "c.table_catalog"
+            cluster_source = "current_database()"
         else:
             cluster_source = f"'{self._cluster}'"
 
         return """
-        SELECT
-          {cluster_source} as cluster, c.table_schema as schema, c.table_name as name, pgtd.description as description
-          ,c.column_name as col_name, c.data_type as col_type
-          , pgcd.description as col_description, ordinal_position as col_sort_order
-        FROM INFORMATION_SCHEMA.COLUMNS c
-        INNER JOIN
-          pg_catalog.pg_statio_all_tables as st on c.table_schema=st.schemaname and c.table_name=st.relname
-        LEFT JOIN
-          pg_catalog.pg_description pgcd on pgcd.objoid=st.relid and pgcd.objsubid=c.ordinal_position
-        LEFT JOIN
-          pg_catalog.pg_description pgtd on pgtd.objoid=st.relid and pgtd.objsubid=0
-        {where_clause_suffix}
-        ORDER by cluster, schema, name, col_sort_order ;
+            SELECT
+                {cluster_source} as cluster,
+                st.schemaname as schema,
+                st.relname as name,
+                pgtd.description as description,
+                att.attname as col_name,
+                pgtyp.typname as col_type,
+                pgcd.description as col_description,
+                att.attnum as col_sort_order
+            FROM pg_catalog.pg_attribute att
+            INNER JOIN
+                pg_catalog.pg_statio_all_tables as st
+                on att.attrelid=st.relid
+            LEFT JOIN
+                pg_catalog.pg_type pgtyp
+                on pgtyp.oid=att.atttypid
+            LEFT JOIN
+                pg_catalog.pg_description pgtd
+                on pgtd.objoid=st.relid and pgtd.objsubid=0
+            LEFT JOIN
+              pg_catalog.pg_description pgcd
+              on pgcd.objoid=st.relid and pgcd.objsubid=att.attnum
+            WHERE att.attnum >=0 and {where_clause_suffix}
+            ORDER by cluster, schema, name, col_sort_order;
         """.format(
             cluster_source=cluster_source,
             where_clause_suffix=where_clause_suffix,
