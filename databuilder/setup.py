15d1e83e25a760ca8c0cfd55b112e9fe16172696
Ben Rifkind <benrifkind@users.noreply.github.com>
2021-07-22 19:25:40 -0600

    feat: Add Salesforce extractor (#1370)
    * SalesForce extractor
    
    Signed-off-by: benrifkind <ben.rifkind@gmail.com>
    
    * Add simple-salesforce dependency
    
    Signed-off-by: benrifkind <ben.rifkind@gmail.com>
    
    * lint tests
    
    Signed-off-by: benrifkind <ben.rifkind@gmail.com>
    
    * Add license header
    
    Signed-off-by: benrifkind <ben.rifkind@gmail.com>
    
    * Add documentation
    
    Signed-off-by: benrifkind <ben.rifkind@gmail.com>
    
    * Add salesforce to all_deps
    
    Signed-off-by: benrifkind <ben.rifkind@gmail.com>
    
    * fix lint
    
    Signed-off-by: benrifkind <ben.rifkind@gmail.com>
    
    * isort
    
    Signed-off-by: benrifkind <ben.rifkind@gmail.com>
    
    * Actually fix issues
    
    Signed-off-by: benrifkind <ben.rifkind@gmail.com>

--- databuilder/setup.py
+++ databuilder/setup.py
@@ -79,8 +79,12 @@ rds = [
     'mysqlclient>=1.3.6,<3'
 ]
 
+salesforce = [
+    'simple-salesforce>=1.11.2'
+]
+
 all_deps = requirements + requirements_dev + kafka + cassandra + glue + snowflake + athena + \
-    bigquery + jsonpath + db2 + dremio + druid + spark + feast + neptune + rds + atlas
+    bigquery + jsonpath + db2 + dremio + druid + spark + feast + neptune + rds + atlas + salesforce
 
 setup(
     name='amundsen-databuilder',
@@ -111,7 +115,8 @@ setup(
         'delta': spark,
         'feast': feast,
         'atlas': atlas,
-        'rds': rds
+        'rds': rds,
+        'salesforce': salesforce,
     },
     classifiers=[
         'Programming Language :: Python :: 3.6',
