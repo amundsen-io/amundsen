945f594928963bd5355c4db7cfbf00cd61b77c7d
Allison Suarez Miranda <asuarezmiranda@lyft.com>
2021-08-06 12:36:44 -0700

    fix: change back FE healthcheck
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>

--- frontend/amundsen_application/api/__init__.py
+++ frontend/amundsen_application/api/__init__.py
@@ -8,15 +8,13 @@ from flask import Flask, render_template
 import jinja2
 import os
 
-from amundsen_application.api.healthcheck import run_healthcheck
-
 
 ENVIRONMENT = os.getenv('APPLICATION_ENV', 'development')
 LOGGER = logging.getLogger(__name__)
 
 
 def init_routes(app: Flask) -> None:
-    app.add_url_rule('/healthcheck', 'healthcheck', run_healthcheck)
+    app.add_url_rule('/healthcheck', 'healthcheck', healthcheck)
     app.add_url_rule('/', 'index', index, defaults={'path': ''})  # also functions as catch_all
     app.add_url_rule('/<path:path>', 'index', index)  # catch_all
 
@@ -27,3 +25,6 @@ def index(path: str) -> Any:
     except jinja2.exceptions.TemplateNotFound as e:
         LOGGER.error("index.html template not found, have you built the front-end JS (npm run build in static/?")
         raise e
+
+def healthcheck() -> Tuple[str, int]:
+    return '', 200  # pragma: no cover
