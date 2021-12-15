3bccd30f5b9836feda55777e8f865439040e887d
Allison Suarez Miranda <asuarezmiranda@lyft.com>
2021-12-15 14:48:43 -0800

    removed check
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>

--- search/search_service/proxy/es_search_proxy.py
+++ search/search_service/proxy/es_search_proxy.py
@@ -376,10 +376,6 @@ class ElasticsearchProxy():
         response = self.elasticsearch.update(index=resource_index,
                                              id=document_id,
                                              body=partial_document)
-        if response.get('result') == 'noop':
-            msg = f'ES _update request returned noop result, field "{field}" was not updated.'
-            LOGGER.error(msg)
-            raise ElasticsearchException(msg)
 
     def update_document_field(self, *,
                               resource_key: str,
