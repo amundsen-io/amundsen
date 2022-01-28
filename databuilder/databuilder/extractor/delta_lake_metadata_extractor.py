e3eaaf80ca8fceccb93ba1162844f358cd1cad28
Harm Weites <harm@weites.com>
2022-01-28 12:23:32 +0100

    Cut the line length
    Signed-off-by: Harm Weites <harm@weites.com>

--- databuilder/databuilder/extractor/delta_lake_metadata_extractor.py
+++ databuilder/databuilder/extractor/delta_lake_metadata_extractor.py
@@ -432,7 +432,8 @@ class DeltaLakeMetadataExtractor(Extractor):
     def is_map_type(self, delta_type: Any) -> bool:
         return isinstance(delta_type, MapType)
 
-    def create_table_watermarks(self, table: ScrapedTableMetadata) -> Union[List[Tuple[Optional[Watermark], Optional[Watermark]]], None]:
+    def create_table_watermarks(self, table: ScrapedTableMetadata) -> Union[List[Tuple[Optional[Watermark],
+                                                                                       Optional[Watermark]]], None]:
         """
         Creates the watermark objects that reflect the highest and lowest values in the partition columns
         """
