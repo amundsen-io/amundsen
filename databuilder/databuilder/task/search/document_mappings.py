88c08e536c893dec4af8a140865c64d9d6b4ddc4
Allison Suarez Miranda <22477579+allisonsuarez@users.noreply.github.com>
2022-02-10 17:01:32 -0800

    feat: general document mappings for ES (#1637)
    * feat: general document mappings for ES
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * notes and chnages to come on what is general vs resource specific mapping
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * functional task to extract and publish search metadata using elasticsearch-dsl document for mappings
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * missing new line at eof
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * missing new line at eof
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * small fixes
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * rewrote config to add custom index and mappings
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * revert neo4j search data extractor change
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * removed whitespace
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * defaul config set as fallback
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * type check for document_mapping
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * lint
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * added ds and uuic to new index withinn task
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * lint
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * more lint
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * fixes and rolled back some chnages from feedback
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * lint
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * bumped library version to 6.5.0
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * space missing
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * mypy fixes
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * had to rename generator function
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * oops
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * isort
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * fixed mapping name mismatch
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * fixed mappings, added timeouts, have keyword sub field for all filter fields
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * lint plus analyzer aplhanum rename
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * this default behavior for mapping selection makes more sense
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * most recent feedback implemented except 2 commentd
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * lint
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * changed analyzer for display name no stemming
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * lint
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * lint
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * fixed delete old index
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * fixed delete old index
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * lint
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * fixed improts
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    
    * removed empty ()
    
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>
    Signed-off-by: Zachary Ruiz <zacharyruiz@Zacharys-MacBook-Pro-2.local>

---
+++ databuilder/databuilder/task/search/document_mappings.py
@@ -0,0 +1,147 @@
+# Copyright Contributors to the Amundsen project.
+# SPDX-License-Identifier: Apache-2.0
+
+from typing import Dict
+
+from elasticsearch_dsl import (
+    Date, Document, Keyword, RankFeatures, Text, analysis, token_filter, tokenizer,
+)
+
+
+class Tokenizer:
+    # separate tokens on all non-alphanumeric characters and whitespace
+    alphanum_tokenizer = tokenizer("alphanum_tokenizer",
+                                   type="pattern",
+                                   pattern="[^a-zA-Z0-9]")
+
+
+class Filter:
+    english_stop = token_filter("english_stop", type="stop", stopwords="_english_")
+
+    # uses default porter stemmer
+    english_stemmer = token_filter("english_stemmer", type="stemmer", language="english")
+
+    english_possessive_stemmer = token_filter("english_possessive_stemmer",
+                                              type="stemmer",
+                                              language="possessive_english")
+
+
+class Analyzer:
+    # tokenizes and makes the tokens lowercase
+    general_analyzer = analysis.analyzer("general_analyzer",
+                                         tokenizer=Tokenizer.alphanum_tokenizer,
+                                         filter=["lowercase"])
+
+    # provides light stemming for english tokens
+    stemming_analyzer = analysis.analyzer("stemming_analyzer",
+                                          tokenizer=Tokenizer.alphanum_tokenizer,
+                                          filter=["lowercase", "kstem"])
+
+    # uses grammar based tokenization before analysis (e.g. "it's fine" -> ["it's", "fine"])
+    english_analyzer = analysis.analyzer("english_analyzer",
+                                         tokenizer=tokenizer("standard_tokenizer",
+                                                             type="standard"),
+                                         filter=[Filter.english_possessive_stemmer,
+                                                 "lowercase",
+                                                 Filter.english_stop,
+                                                 Filter.english_stemmer])
+
+    # tokenizes for words and numbers, removing all other characters before analysis
+    # (e.g. "it's fine" -> ["it", "s", "fine"] or "hello_word" -> ["hello", "world"])
+    alphanum_analyzer = analysis.analyzer("alphanum_analyzer",
+                                          tokenizer=Tokenizer.alphanum_tokenizer,
+                                          filter=[Filter.english_possessive_stemmer,
+                                                  "lowercase",
+                                                  Filter.english_stop,
+                                                  Filter.english_stemmer])
+
+# Resource Mappings
+
+
+class SearchableResource(Document):
+    # For better understanding of field type rationale read "Mapping unstructured content"
+    # https://www.elastic.co/guide/en/elasticsearch/reference/current/keyword.html#wildcard-field-type
+    key = Text(required=True,
+               fields={"keyword": Keyword()},
+               analyzer=Analyzer.general_analyzer)
+    name = Text(required=True,
+                fields={"keyword": Keyword()},
+                analyzer=Analyzer.stemming_analyzer)
+    description = Text(analyzer=Analyzer.english_analyzer,
+                       fields={"alphanumeric": Text(analyzer=Analyzer.alphanum_analyzer)})
+    badges = Text(multi=True,
+                  fields={"keyword": Keyword()},
+                  analyzer=Analyzer.general_analyzer)
+    tags = Text(multi=True,
+                fields={"keyword": Keyword()},
+                analyzer=Analyzer.general_analyzer)
+    usage = RankFeatures()  # values can be used to boost document search score
+    last_updated_timestamp = Date()
+
+
+class Table(SearchableResource):
+    display_name = Text(required=True,
+                        fields={"keyword": Keyword()},
+                        analyzer=Analyzer.general_analyzer)
+    database = Text(required=True,
+                    fields={"keyword": Keyword()},
+                    analyzer=Analyzer.general_analyzer)
+    cluster = Text(required=True,
+                   fields={"keyword": Keyword()},
+                   analyzer=Analyzer.general_analyzer)
+    schema = Text(required=True,
+                  fields={"keyword": Keyword()},
+                  analyzer=Analyzer.stemming_analyzer)
+    columns = Text(multi=True,
+                   fields={"keyword": Keyword()},
+                   analyzer=Analyzer.stemming_analyzer)
+    column_descriptions = Text(multi=True,
+                               fields={
+                                   "alphanumeric": Text(analyzer=Analyzer.alphanum_analyzer)},
+                               analyzer=Analyzer.english_analyzer)
+
+
+class Dashboard(SearchableResource):
+    group_name = Text(required=True,
+                      fields={"keyword": Keyword()},
+                      analyzer=Analyzer.stemming_analyzer)
+    group_description = Text(analyzer=Analyzer.english_analyzer)
+    query_names = Text(multi=True,
+                       fields={"keyword": Keyword()},
+                       analyzer=Analyzer.stemming_analyzer)
+    chart_names = Text(multi=True,
+                       fields={"keyword": Keyword()},
+                       analyzer=Analyzer.stemming_analyzer)
+
+
+class Feature(SearchableResource):
+    feature_group = Text(required=True,
+                         fields={"keyword": Keyword()},
+                         analyzer=Analyzer.stemming_analyzer)
+    version = Keyword(required=True)
+    status = Keyword()
+    entity = Text(multi=True,
+                  fields={"keyword": Keyword()},
+                  analyzer=Analyzer.general_analyzer)
+    availability = Keyword()
+
+
+class User(SearchableResource):
+    # key is email
+    # name is full name, no separate first and last name
+    # total read, total own, total follow goes under usage metrics
+    first_name = Text(required=True,
+                      fields={"keyword": Keyword()},
+                      analyzer=Analyzer.stemming_analyzer)
+    last_name = Text(required=True,
+                     fields={"keyword": Keyword()},
+                     analyzer=Analyzer.stemming_analyzer)
+
+
+RESOURCE_TO_MAPPING: Dict[str, Document] = {
+    'table': Table,
+    'dashboard': Dashboard,
+    'feature': Feature,
+    'user': User,
+    'base': SearchableResource,
+}
