fd45e2323ffe4474f6d7948fc0433062c81d8e35
Kristen Armes <6732445+kristenarmes@users.noreply.github.com>
2022-03-17 15:28:08 -0700

    refactor: Change error handling in ComplexTypeTransformer (#1766)
    * Log issues instead of throwing exceptions in ComplexTypeTransformer
    
    Signed-off-by: Kristen Armes <karmes@lyft.com>
    
    * Bump version
    
    Signed-off-by: Kristen Armes <karmes@lyft.com>
    
    * Handle any exceptions with the parser in the transformer instead of supressing them so it knows when there is an issue
    
    Signed-off-by: Kristen Armes <karmes@lyft.com>
    
    * lint
    
    Signed-off-by: Kristen Armes <karmes@lyft.com>
    
    * Don't catch exception for invalid parsing function
    
    Signed-off-by: Kristen Armes <karmes@lyft.com>
    
    * No special handling for invalid parsing function
    
    Signed-off-by: Kristen Armes <karmes@lyft.com>
    Signed-off-by: Zachary Ruiz <zacharyruiz@Zacharys-MacBook-Pro-2.local>

--- databuilder/databuilder/transformer/complex_type_transformer.py
+++ databuilder/databuilder/transformer/complex_type_transformer.py
@@ -2,14 +2,18 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import importlib
+import logging
 from typing import Any
 
 from pyhocon import ConfigTree
 
 from databuilder.models.table_metadata import TableMetadata
+from databuilder.models.type_metadata import ScalarTypeMetadata
 from databuilder.transformer.base_transformer import Transformer
 
 PARSING_FUNCTION = 'parsing_function'
+
+LOGGER = logging.getLogger(__name__)
 
 
 class ComplexTypeTransformer(Transformer):
@@ -21,21 +25,29 @@ class ComplexTypeTransformer(Transformer):
     field with the parsed results contained in a TypeMetadata object.
     """
     def init(self, conf: ConfigTree) -> None:
-        try:
-            parsing_function = conf.get_string(PARSING_FUNCTION)
-            module_name, function_name = parsing_function.rsplit(".", 1)
-            mod = importlib.import_module(module_name)
-            self._parsing_function = getattr(mod, function_name)
-        except (ValueError, ModuleNotFoundError, AttributeError):
-            raise Exception("Invalid parsing function provided to ComplexTypeTransformer")
+        self.success_count = 0
+        self.failure_count = 0
+
+        parsing_function = conf.get_string(PARSING_FUNCTION)
+        module_name, function_name = parsing_function.rsplit(".", 1)
+        mod = importlib.import_module(module_name)
+        self._parsing_function = getattr(mod, function_name)
 
     def transform(self, record: Any) -> TableMetadata:
         if not isinstance(record, TableMetadata):
-            raise Exception("ComplexTypeTransformer expects record of type TableMetadata")
+            raise Exception(f"ComplexTypeTransformer expects record of type TableMetadata, received {type(record)}")
 
         for column in record.columns:
-            column.set_column_key(record._get_col_key(column))
-            column.set_type_metadata(self._parsing_function(column.type, column.name, column))
+            try:
+                column.set_column_key(record._get_col_key(column))
+                column.set_type_metadata(self._parsing_function(column.type, column.name, column))
+            except Exception as e:
+                # Default to scalar type if the type string cannot be parsed
+                column.set_type_metadata(ScalarTypeMetadata(name=column.name, parent=column, type_str=column.type))
+                self.failure_count += 1
+                LOGGER.warning(f"Could not parse type for column {column.name} in table {record.name}: {e}")
+            else:
+                self.success_count += 1
 
         return record
 
