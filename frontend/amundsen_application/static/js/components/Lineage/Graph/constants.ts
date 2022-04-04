7ce2153c3b815394a20b473237e1b701ac17655b
Ozan Dogrultan <ozan.dogrultan@deliveryhero.com>
2022-04-04 06:08:19 +0200

    fix: Add schema name to Lineage graph node labels (#1792)
    * fix: Add schema name to Lineage graph node labels
    
    Signed-off-by: Ozan Dogrultan <ozan.dogrultan@deliveryhero.com>
    
    * fix: Add getNodeLabel function
    
    Signed-off-by: Ozan Dogrultan <ozan.dogrultan@deliveryhero.com>
    
    * fix: Pass getNodeLabel without arguments
    
    Signed-off-by: Ozan Dogrultan <ozan.dogrultan@deliveryhero.com>
    Signed-off-by: Zachary Ruiz <zacharyruiz@Zacharys-MacBook-Pro-2.local>

--- frontend/amundsen_application/static/js/components/Lineage/Graph/constants.ts
+++ frontend/amundsen_application/static/js/components/Lineage/Graph/constants.ts
@@ -6,7 +6,7 @@ export const LINEAGE_SCENE_MARGIN = {
 };
 export const CHART_DEFAULT_DIMENSIONS = { width: 1280, height: 1024 };
 export const CHART_DEFAULT_LABELS = { upstream: '', downstream: '' };
-export const UPSTREAM_LABEL_OFFSET = 150;
+export const UPSTREAM_LABEL_OFFSET = 200;
 export const NODE_STATUS_Y_OFFSET = '.30em';
 export const NODE_LABEL_Y_OFFSET = 25;
 export const ANIMATION_DURATION = 750;
