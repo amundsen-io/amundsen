ef1a57040577f744be72c0813da2db62f8155a3b
Allison Suarez Miranda <asuarezmiranda@lyft.com>
2022-04-27 12:49:55 -0700

    updated index name to point to new index
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>

--- search/search_service/proxy/es_search_proxy.py
+++ search/search_service/proxy/es_search_proxy.py
@@ -129,7 +129,7 @@ class ElasticsearchProxy():
 
     def get_index_for_resource(self, resource_type: Resource) -> str:
         resource_str = resource_type.name.lower()
-        return f"{resource_str}_search_index"
+        return f"new_{resource_str}_search_index"
 
     def _build_must_query(self, resource: Resource, query_term: str) -> List[Q]:
         """
@@ -369,6 +369,7 @@ class ElasticsearchProxy():
                         # mapping gives all the fields in the response
                         result = {}
                         fields = self.RESOUCE_TO_MAPPING[Resource[resource_type.upper()]]
+                        print(fields)
                         for f in fields.keys():
                             # remove "keyword" from mapping value
                             field = fields[f].split('.')[0]
