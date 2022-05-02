2b3b717ed23e5d7d98bf7a33a9a1fb4f844d7a5c
Miquel Perelló García <miquel.perello.garcia@revolut.com>
2022-05-02 12:52:35 +0200

    Add comments to initial commit

--- frontend/amundsen_application/api/utils/metadata_utils.py
+++ frontend/amundsen_application/api/utils/metadata_utils.py
@@ -114,6 +114,8 @@ def marshall_table_full(table_dict: Dict) -> Dict:
     results: Dict[str, Any] = schema.dump(table)
 
     # is_editable = is_table_editable(results['schema'], results['name'])
+    # Forcing to False since we don't want to allow the users to update
+    # any parameters through the UI
     is_editable = False
     results['is_editable'] = is_editable
 
