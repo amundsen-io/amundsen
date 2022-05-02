7558652728d0c87fb55803829e1652245362dcce
Allison Suarez Miranda <asuarezmiranda@lyft.com>
2022-05-02 12:40:45 -0700

    updated all unit tests and initialize proxy classes according to bww comp functionality
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>

--- search/tests/unit/proxy/v2/test_es_proxy_v2.py
+++ search/tests/unit/proxy/v2/test_es_proxy_v2.py
@@ -2,26 +2,39 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import unittest
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 
 from amundsen_common.models.search import Filter, SearchResponse
 from elasticsearch_dsl import Search
 from elasticsearch_dsl.response import Response
 
 from search_service import create_app
-from search_service.proxy.es_proxy_v3 import ElasticsearchProxy, Resource
-from tests.unit.proxy.fixtures import (
+from search_service.proxy.es_proxy_v2 import ElasticsearchProxyV2, Resource
+from tests.unit.proxy.v2.fixtures_v2 import (
     FILTER_QUERY, RESPONSE_1, RESPONSE_2, TERM_FILTERS_QUERY, TERM_QUERY,
 )
 
 
-class TestElasticsearchProxy(unittest.TestCase):
+class TestElasticsearchProxyV2(unittest.TestCase):
     def setUp(self) -> None:
         self.app = create_app(config_module_class='search_service.config.LocalConfig')
         self.app_context = self.app.app_context()
         self.app_context.push()
+        mock_index = 'mock_index'
         mock_elasticsearch_client = MagicMock()
-        self.es_proxy = ElasticsearchProxy(client=mock_elasticsearch_client)
+        mock_elasticsearch_client.indices.get_alias.return_value = {
+            mock_index: {}
+        }
+        mock_elasticsearch_client.indices.get_mapping.return_value = {
+            mock_index: {
+                'mappings': {}
+            } 
+        }
+        self.es_proxy = ElasticsearchProxyV2(host='mock_host',
+                                             user='mock_user',
+                                             password='mock_password',
+                                             client=mock_elasticsearch_client,
+                                             page_size=10)
 
     def test_build_elasticsearch_query_term_filters(self) -> None:
         actual = self.es_proxy._build_elasticsearch_query(resource=Resource.FEATURE,
