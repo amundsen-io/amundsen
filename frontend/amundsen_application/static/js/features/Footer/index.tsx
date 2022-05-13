64b0af610068a0cfd5f6463b69a82338fe4ccd0a
Zachary Ruiz <zac.ruiz@salt.io>
2022-05-12 22:38:16 -0400

    Revert "Content tweaks"

--- frontend/amundsen_application/static/js/features/Footer/index.tsx
+++ frontend/amundsen_application/static/js/features/Footer/index.tsx
@@ -11,8 +11,6 @@ import { GetLastIndexedRequest } from 'ducks/lastIndexed/types';
 
 import { formatDateTimeLong } from 'utils/dateUtils';
 
-import { getFooterContentHtml } from 'config/config-utils';
-
 import './styles.scss';
 
 // Props
@@ -53,10 +51,6 @@ export class Footer extends React.Component<FooterProps> {
           )}`}
         </div>
       );
-    } else {
-      content = (
-        <div dangerouslySetInnerHTML={{ __html: getFooterContentHtml() }} />
-      )
     }
 
     return (
