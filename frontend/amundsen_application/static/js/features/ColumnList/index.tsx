30cf377ef9d4516cc36f4c7cf08fc5abc4d77461
Kristen Armes <6732445+kristenarmes@users.noreply.github.com>
2022-05-26 14:04:32 -0700

    feat: Nested columns special type rows and expand by default (#1872)
    * Special type handling rows for nested columns, and expand nested by default unless max config is reached
    
    Signed-off-by: Kristen Armes <karmes@lyft.com>
    
    * Using constant for row height
    
    Signed-off-by: Kristen Armes <karmes@lyft.com>
    
    * Addressing PR comments
    
    Signed-off-by: Kristen Armes <karmes@lyft.com>
    
    * Passing max number of nested cols as an option to Table instead of from config
    
    Signed-off-by: Kristen Armes <karmes@lyft.com>

--- frontend/amundsen_application/static/js/features/ColumnList/index.tsx
+++ frontend/amundsen_application/static/js/features/ColumnList/index.tsx
@@ -417,6 +417,7 @@ const ColumnList: React.FC<ColumnListProps> = ({
     tableParams,
     index,
     isNestedColumn: true,
+    kind: item.kind,
   });
 
   return (
@@ -433,6 +434,7 @@ const ColumnList: React.FC<ColumnListProps> = ({
         preExpandPanelKey,
         currentSelectedKey,
         tableKey,
+        maxNumRows: getMaxNestedColumns(),
       }}
     />
   );
