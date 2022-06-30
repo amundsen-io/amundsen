7e5ee0e6ddca2b56deee7c10c1a5d186cab87e4f
Ozan Dogrultan <ozan.dogrultan@deliveryhero.com>
2022-06-30 13:51:35 +0200

    fix: Update bounds for databuilder google-auth versions
    Signed-off-by: Ozan Dogrultan <ozan.dogrultan@deliveryhero.com>

--- databuilder/setup.py
+++ databuilder/setup.py
@@ -36,7 +36,7 @@ athena = ['PyAthena[SQLAlchemy]>=1.0.0, <2.0.0']
 bigquery = [
     'google-api-python-client>=1.6.0, <2.0.0dev',
     'google-auth-httplib2>=0.0.1',
-    'google-auth>=2.6.0'
+    'google-auth>=1.16.0, <3.0.0dev'
 ]
 
 jsonpath = ['jsonpath_rw==1.4.0']
