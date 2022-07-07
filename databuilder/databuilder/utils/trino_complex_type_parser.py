11f1f6458e55c83175d6f65708cd3d5f615e8f50
Kristen Armes <6732445+kristenarmes@users.noreply.github.com>
2022-07-07 10:48:49 -0700

    feat: Adding new Trino type parser and other type metadata updates (#1917)
    * Adding new Trino type parser and other type metadata updates
    
    Signed-off-by: Kristen Armes <karmes@lyft.com>
    
    * Revert change to _format_as_list function name
    
    Signed-off-by: Kristen Armes <karmes@lyft.com>
    
    * Updating warning logging messages for when badges and descriptions aren't supported
    
    Signed-off-by: Kristen Armes <karmes@lyft.com>
    
    * lint
    
    Signed-off-by: Kristen Armes <karmes@lyft.com>
    
    * Bump databuilder version
    
    Signed-off-by: Kristen Armes <karmes@lyft.com>

--- databuilder/databuilder/utils/trino_complex_type_parser.py
+++ databuilder/databuilder/utils/trino_complex_type_parser.py
@@ -14,33 +14,28 @@ from databuilder.models.type_metadata import (
 
 array_keyword = Keyword("array")
 map_keyword = Keyword("map")
-struct_keyword = Keyword("struct")
-union_keyword = Keyword("uniontype")
+struct_keyword = Keyword("row")
 
 field_name = Word(alphanums + "_")
 field_type = Forward()
 
 # Scalar types
-union_list = delimitedList(field_type)
-union_type = nestedExpr(
-    opener=union_keyword + "<", closer=">", content=union_list, ignoreExpr=None
-)
 scalar_quantifier = "(" + Word(nums) + Optional(")" | "," + Word(nums) + ")")
-scalar_type = union_type | OneOrMore(Word(alphanums + "_")) + Optional(scalar_quantifier)
+scalar_type = OneOrMore(Word(alphanums + "_")) + Optional(scalar_quantifier)
 
 # Complex types
-array_field = "<" + field_type("type")
+array_field = "(" + field_type("type")
 map_field = originalTextFor(scalar_type)("key") + "," + field_type("type")
-struct_field = field_name("name") + ":" + field_type("type")
+struct_field = field_name("name") + field_type("type")
 struct_list = delimitedList(Group(struct_field))
 array_type = nestedExpr(
-    opener=array_keyword, closer=">", content=array_field, ignoreExpr=None
+    opener=array_keyword, closer=")", content=array_field, ignoreExpr=None
 )
 map_type = nestedExpr(
-    opener=map_keyword + "<", closer=">", content=map_field, ignoreExpr=None
+    opener=map_keyword + "(", closer=")", content=map_field, ignoreExpr=None
 )
 struct_type = nestedExpr(
-    opener=struct_keyword + "<", closer=">", content=struct_list, ignoreExpr=None
+    opener=struct_keyword + "(", closer=")", content=struct_list, ignoreExpr=None
 )
 
 field_type <<= originalTextFor(array_type | map_type | struct_type | scalar_type)
@@ -49,8 +44,9 @@ complex_type = (array_type("array_type") | map_type("map_type") | struct_type("s
                 scalar_type("scalar_type"))
 
 
-def parse_hive_type(type_str: str, name: str, parent: Union[ColumnMetadata, TypeMetadata]) -> TypeMetadata:
+def parse_trino_type(type_str: str, name: str, parent: Union[ColumnMetadata, TypeMetadata]) -> TypeMetadata:
     type_str = type_str.lower()
+    type_str = type_str.replace('\"', '')  # Remove quotes around names that are added when querying from HMS
     parsed_type = complex_type.parseString(type_str, parseAll=True)
 
     if parsed_type.scalar_type:
@@ -63,7 +59,7 @@ def parse_hive_type(type_str: str, name: str, parent: Union[ColumnMetadata, Type
         array_type_metadata = ArrayTypeMetadata(name=name,
                                                 parent=parent,
                                                 type_str=type_str)
-        array_inner_type = parse_hive_type(results.type, '_inner_', array_type_metadata)
+        array_inner_type = parse_trino_type(results.type, '_inner_', array_type_metadata)
         if not isinstance(array_inner_type, ScalarTypeMetadata):
             array_type_metadata.array_inner_type = array_inner_type
         return array_type_metadata
@@ -71,8 +67,8 @@ def parse_hive_type(type_str: str, name: str, parent: Union[ColumnMetadata, Type
         map_type_metadata = MapTypeMetadata(name=name,
                                             parent=parent,
                                             type_str=type_str)
-        map_type_metadata.map_key_type = parse_hive_type(results.key, '_map_key', map_type_metadata)
-        map_type_metadata.map_value_type = parse_hive_type(results.type, '_map_value', map_type_metadata)
+        map_type_metadata.map_key_type = parse_trino_type(results.key, '_map_key', map_type_metadata)
+        map_type_metadata.map_value_type = parse_trino_type(results.type, '_map_value', map_type_metadata)
         return map_type_metadata
     elif parsed_type.struct_type:
         struct_type_metadata = StructTypeMetadata(name=name,
@@ -80,7 +76,7 @@ def parse_hive_type(type_str: str, name: str, parent: Union[ColumnMetadata, Type
                                                   type_str=type_str)
         struct_items = {}
         for index, result in enumerate(results):
-            struct_items[result.name] = parse_hive_type(result.type, result.name, struct_type_metadata)
+            struct_items[result.name] = parse_trino_type(result.type, result.name, struct_type_metadata)
             struct_items[result.name].sort_order = index
 
         struct_type_metadata.struct_items = struct_items
