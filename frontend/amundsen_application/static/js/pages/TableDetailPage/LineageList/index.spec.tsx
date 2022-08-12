7ca2325f35d112ef1c1e521b9b82bee8f0b0a700
Ozan Dogrultan <ozan.dogrultan@deliveryhero.com>
2022-08-12 15:53:31 +0200

    feat: Add LineageList test
    Signed-off-by: Ozan Dogrultan <ozan.dogrultan@deliveryhero.com>

---
+++ frontend/amundsen_application/static/js/pages/TableDetailPage/LineageList/index.spec.tsx
@@ -0,0 +1,35 @@
+// Copyright Contributors to the Amundsen project.
+// SPDX-License-Identifier: Apache-2.0
+
+import * as React from 'react';
+import { shallow } from 'enzyme';
+import LineageList from './index';
+
+describe('LineageList', () => {
+  let wrapper;
+  let props;
+  beforeAll(() => {
+    props = {
+      items: [
+        {
+          badges: [],
+          cluster: 'gold',
+          database: 'hive',
+          key: 'hive://gold.test_schema/test_table1',
+          level: 0,
+          name: 'test_table1',
+          parent: null,
+          schema: 'test_schema',
+          source: 'hive',
+          usage: 0,
+        },
+      ],
+      direction: 'both',
+    };
+    wrapper = shallow<typeof LineageList>(<LineageList {...props} />);
+  });
+
+  it('should render a link', () => {
+    expect(wrapper.find('a').exists).toBeTruthy();
+  });
+});
