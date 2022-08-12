16bae664e1f6dafbe3469b21b3ff6f56fa5367c7
Ozan Dogrultan <ozan.dogrultan@deliveryhero.com>
2022-08-12 17:56:33 +0200

    feat: Refactor LineageList test
    Signed-off-by: Ozan Dogrultan <ozan.dogrultan@deliveryhero.com>

--- frontend/amundsen_application/static/js/pages/TableDetailPage/LineageList/index.spec.tsx
+++ frontend/amundsen_application/static/js/pages/TableDetailPage/LineageList/index.spec.tsx
@@ -3,37 +3,43 @@
 
 import * as React from 'react';
 import { shallow } from 'enzyme';
-import LineageList from './index';
+import LineageList, { LineageListProps } from './index';
+
+const setup = (propOverrides?: Partial<LineageListProps>) => {
+  const props: LineageListProps = {
+    items: [
+      {
+        badges: [],
+        cluster: 'gold',
+        database: 'hive',
+        key: 'hive://gold.test_schema/test_table1',
+        level: 0,
+        name: 'test_table1',
+        parent: null,
+        schema: 'test_schema',
+        source: 'hive',
+        usage: 0,
+      },
+    ],
+    direction: 'both',
+    ...propOverrides,
+  };
+  const wrapper = shallow<typeof LineageList>(<LineageList {...props} />);
+
+  return {
+    props,
+    wrapper,
+  };
+};
 
 describe('LineageList', () => {
-  let wrapper;
-  let props;
-  beforeAll(() => {
-    props = {
-      items: [
-        {
-          badges: [],
-          cluster: 'gold',
-          database: 'hive',
-          key: 'hive://gold.test_schema/test_table1',
-          level: 0,
-          name: 'test_table1',
-          parent: null,
-          schema: 'test_schema',
-          source: 'hive',
-          usage: 0,
-        },
-      ],
-      direction: 'both',
-    };
-    wrapper = shallow<typeof LineageList>(<LineageList {...props} />);
-  });
-
   it('should render a link', () => {
+    const { wrapper } = setup();
     expect(wrapper.find('a').exists).toBeTruthy();
   });
 
   it('should have a disabled link', () => {
+    const { wrapper } = setup();
     jest.mock('./index', () => ({
       isTableLinkDisabled: jest.fn().mockReturnValueOnce(true),
     }));
