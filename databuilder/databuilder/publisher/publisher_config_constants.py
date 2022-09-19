261134a07956304206b0681972267a631292a59a
Kristen Armes <6732445+kristenarmes@users.noreply.github.com>
2022-09-19 14:07:59 -0700

    feat: Add configurable prop types to neo4j csv publisher (#1993)
    * Add configurable prop types to neo4j csv publisher
    
    Signed-off-by: Kristen Armes <karmes@lyft.com>
    
    * Bump databuilder version
    
    Signed-off-by: Kristen Armes <karmes@lyft.com>
    
    Signed-off-by: Kristen Armes <karmes@lyft.com>

--- databuilder/databuilder/publisher/publisher_config_constants.py
+++ databuilder/databuilder/publisher/publisher_config_constants.py
@@ -64,3 +64,10 @@ class Neo4jCsvPublisherConfigs:
     # NEO4J_VALIDATE_SSL is a boolean indicating whether to validate the server's SSL/TLS
     # cert against system CAs
     NEO4J_VALIDATE_SSL = 'neo4j_validate_ssl'
+
+    # This should be a dict using property names as keys mapped to the function name used to configure a specific
+    # type for that property. The values of the properties should be in the correct format that the function accepts.
+    # Example: a config of {'start_time': 'datetime', 'publish_tag': 'date'} where the property values are in the
+    # format <date>T<time> and <date> would apply datetime(n.start_time) and date(n.publish_tag) in the prop merge
+    # statement to create the props as DateTime and Date types instead of strings.
+    NEO4J_PROP_TYPES_TO_CONFIGURE = 'neo4j_prop_types_to_configure'
