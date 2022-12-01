5989cbeb22c0cfc64f6c8da641c0283092d699cd
Allison Suarez Miranda <asuarezmiranda@lyft.com>
2022-12-01 13:00:52 -0800

    unit test
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>

--- frontend/tests/unit/api/metadata/test_v0.py
+++ frontend/tests/unit/api/metadata/test_v0.py
@@ -1425,57 +1425,56 @@ class MetadataTest(unittest.TestCase):
                 '/api/metadata/v0/lineage',
                 json={
                     {
-  "downstream_count": 2, 
-  "downstream_entities": [
-    {
-      "badges": None, 
-      "cluster": "cluster", 
-      "database": "hive", 
-      "in_amundsen": None, 
-      "key": "db://cluster.schema/test_downstream_1", 
-      "level": 1, 
-      "link": "/search?resource=table&index=0", 
-      "name": "test_downstream_1", 
-      "parent": None, 
-      "schema": "schema", 
-      "source": "db", 
-      "usage": 1846
-    }, 
-    {
-      "badges": None, 
-      "cluster": "cluster", 
-      "database": "hive", 
-      "in_amundsen": None, 
-      "key": "db://cluster.schema/test_downstream_2", 
-      "level": 1, 
-      "link": "/search?resource=table&index=0", 
-      "name": "test_downstream_2", 
-      "parent": None, 
-      "schema": "schema", 
-      "source": "db", 
-      "usage": 1846
-    }, 
+                        "downstream_count": 2, 
+                        "downstream_entities": [
+                            {
+                            "badges": None, 
+                            "cluster": "cluster", 
+                            "database": "hive", 
+                            "in_amundsen": None, 
+                            "key": "db://cluster.schema/test_downstream_1", 
+                            "level": 1, 
+                            "link": "/search?resource=table&index=0", 
+                            "name": "test_downstream_1", 
+                            "parent": None, 
+                            "schema": "schema", 
+                            "source": "db", 
+                            "usage": 1846
+                            }, 
+                            {
+                            "badges": None, 
+                            "cluster": "cluster", 
+                            "database": "hive", 
+                            "in_amundsen": None, 
+                            "key": "db://cluster.schema/test_downstream_2", 
+                            "level": 1, 
+                            "link": "/search?resource=table&index=0", 
+                            "name": "test_downstream_2", 
+                            "parent": None, 
+                            "schema": "schema", 
+                            "source": "db", 
+                            "usage": 1846
+                            }, 
 
-  ], 
-  "upstream_count": 1, 
-  "upstream_entities": [
-{
-      "badges": None, 
-      "cluster": "cluster", 
-      "database": "hive", 
-      "in_amundsen": None, 
-      "key": "db://cluster.schema/test_upstream_1", 
-      "level": 1, 
-      "link": "/search?resource=table&index=0", 
-      "name": "test_upstream_1", 
-      "parent": None, 
-      "schema": "schema", 
-      "source": "db", 
-      "usage": 1846
-    }, 
-  ]
-}
-
+                        ], 
+                        "upstream_count": 1, 
+                        "upstream_entities": [
+                        {
+                            "badges": None, 
+                            "cluster": "cluster", 
+                            "database": "hive", 
+                            "in_amundsen": None, 
+                            "key": "db://cluster.schema/test_upstream_1", 
+                            "level": 1, 
+                            "link": "/search?resource=table&index=0", 
+                            "name": "test_upstream_1", 
+                            "parent": None, 
+                            "schema": "schema", 
+                            "source": "db", 
+                            "usage": 1846
+                            }, 
+                        ]
+                    }
                 }
             )
             self.assertEqual(response.status_code, HTTPStatus.OK)
