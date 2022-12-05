e7ad6f8bb038da2a08925d2d9b7ed656789cc259
Marcos Iglesias <190833+Golodhros@users.noreply.github.com>
2022-12-05 10:16:58 -0800

    feat: tweaks styling of Alerts (#2043)
    * chore: udpates alert story format; adds severity cases
    
    Signed-off-by: Marcos Iglesias <golodhros@gmail.com>
    
    * feat: updates styling of alert backgrounds
    
    Signed-off-by: Marcos Iglesias <golodhros@gmail.com>
    
    * docs: udpates screenshots
    
    Signed-off-by: Marcos Iglesias <golodhros@gmail.com>
    
    * chore: updating screenshot name
    
    Signed-off-by: Marcos Iglesias <golodhros@gmail.com>
    
    Signed-off-by: Marcos Iglesias <golodhros@gmail.com>

--- frontend/amundsen_application/static/js/components/Alert/index.tsx
+++ frontend/amundsen_application/static/js/components/Alert/index.tsx
@@ -15,13 +15,24 @@ const SEVERITY_TO_COLOR_MAP = {
   [NoticeSeverity.WARNING]: '#ffb146', // $amber50
   [NoticeSeverity.ALERT]: '#b8072c', // $red70
 };
+const SEVERITY_TO_SEVERITY_CLASS = {
+  [NoticeSeverity.INFO]: 'is-info',
+  [NoticeSeverity.WARNING]: 'is-warning',
+  [NoticeSeverity.ALERT]: 'is-alert',
+};
 
 export interface AlertProps {
+  /** Message to show in the alert */
   message: string | React.ReactNode;
+  /** Severity of the alert (info, warning, or alert) */
   severity?: NoticeSeverity;
+  /** Link passed to set as the action (for routing links) */
   actionLink?: React.ReactNode;
+  /** Text of the link action */
   actionText?: string;
+  /** Href for the link action */
   actionHref?: string;
+  /** Callback to call when the action is clicked */
   onAction?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
@@ -37,26 +48,22 @@ const Alert: React.FC<AlertProps> = ({
 
   if (actionText && onAction) {
     action = (
-      <span className="alert-action">
-        <button type="button" className="btn btn-link" onClick={onAction}>
-          {actionText}
-        </button>
-      </span>
+      <button type="button" className="btn btn-link" onClick={onAction}>
+        {actionText}
+      </button>
     );
   }
 
   if (actionText && actionHref) {
     action = (
-      <span className="alert-action">
-        <a className="action-link" href={actionHref}>
-          {actionText}
-        </a>
-      </span>
+      <a className="action-link" href={actionHref}>
+        {actionText}
+      </a>
     );
   }
 
   if (actionLink) {
-    action = <span className="alert-action">{actionLink}</span>;
+    action = actionLink;
   }
 
   let iconComponent: React.ReactNode = null;
@@ -82,10 +89,10 @@ const Alert: React.FC<AlertProps> = ({
     typeof message === 'string' ? <SanitizedHTML html={message} /> : message;
 
   return (
-    <div className="alert">
+    <div className={`alert ${SEVERITY_TO_SEVERITY_CLASS[severity]}`}>
       {iconComponent}
       <p className="alert-message">{formattedMessage}</p>
-      {action}
+      {action && <span className="alert-action">{action}</span>}
     </div>
   );
 };
