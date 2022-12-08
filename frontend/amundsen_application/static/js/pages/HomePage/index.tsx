3479e6492271a9db7853a5e705d43438e17bd764
Ben Dye <bdye@lyft.com>
2022-12-08 18:45:48 -0500

    Get default widgets via the function
    Signed-off-by: Ben Dye <bdye@lyft.com>

--- frontend/amundsen_application/static/js/pages/HomePage/index.tsx
+++ frontend/amundsen_application/static/js/pages/HomePage/index.tsx
@@ -23,6 +23,7 @@ import { Widget } from 'interfaces/Widgets';
 import { SEARCH_BREADCRUMB_TEXT, HOMEPAGE_TITLE } from './constants';
 
 import './styles.scss';
+import { COMMENTS_PLACEHOLDER } from 'features/Feedback/constants';
 
 export interface DispatchFromProps {
   searchReset: () => UpdateSearchStateReset;
@@ -32,44 +33,118 @@ export type HomePageLayout = Widget[];
 
 export type HomePageProps = DispatchFromProps & RouteComponentProps<any>;
 
+const getHomePageWidgets = (layout: HomePageLayout): React.ReactNode[] => {
+  /* Looks up each Widget in layout by its name property, and if found, 
+  puts the relevant component's JSX into the output array. */
+
+  const res: React.ReactNode[] = [];
+
+  layout.forEach((widget) => {
+    switch (widget.name) {
+      // TODO should probably look up the string names via an enum?
+      // TODO the className same for all the widget components?
+      case 'SearchBarWidget': {
+        res.push(
+          <div className="home-element-container">
+            <SearchBarWidget />
+          </div>
+        );
+        break;
+      }
+      // TODO put Breadcrumb inside the search component
+      case 'Breadcrumb': {
+        res.push(
+          <div className="filter-breadcrumb pull-right">
+            <Breadcrumb
+              direction="right"
+              path="/search"
+              text={SEARCH_BREADCRUMB_TEXT}
+            />
+          </div>
+        );
+        break;
+      }
+      case 'BadgesListWidget': {
+        res.push(
+          <div className="home-element-container">
+            <BadgesListWidget shortBadgesList />
+          </div>
+        );
+        break;
+      }
+      case 'TagsListWidget': {
+        res.push(
+          <div className="home-element-container">
+            <TagsListWidget shortTagsList />
+          </div>
+        );
+        break;
+      }
+      case 'MyBookmarksWidget': {
+        res.push(
+          <div className="home-element-container">
+            <MyBookmarksWidget />
+          </div>
+        );
+        break;
+      }
+      case 'PopularResourcesWidget': {
+        res.push(
+          <div className="home-element-container">
+            <PopularResourcesWidget />
+          </div>
+        );
+        break;
+      }
+      default:
+        console.log(`Widget name not found: ${widget.name}`);
+    }
+  });
+
+  return res;
+};
+
+const defaultHomePageLayout: HomePageLayout = [
+  // TODO enums / string constants
+  {
+    name: 'SearchBarWidget',
+    options: {},
+  },
+  // TODO breadcrumb into searchbar
+  {
+    name: 'Breadcrumb',
+    options: {},
+  },
+  {
+    name: 'BadgesListWidget',
+    options: {},
+  },
+  {
+    name: 'TagsListWidget',
+    options: {},
+  },
+  {
+    name: 'MyBookmarksWidget',
+    options: {},
+  },
+  {
+    name: 'PopularResourcesWidget',
+    options: {},
+  },
+];
+
+const HomePageWidgets = (props) => {
+  const { homePageLayout } = props;
+  // TODO for widget in widgets, make a div with the className
+  const widgets = getHomePageWidgets(homePageLayout);
+
+  return <div>{widgets}</div>;
+};
+
 export class HomePage extends React.Component<HomePageProps> {
   componentDidMount() {
     this.props.searchReset();
   }
-
-  getHomePageWidgets(layout: HomePageLayout): React.ReactNode[] {
-    /* TODO: Make 100% sure it returns the pre-existing layout absent a custom config,
-    to not break any OSS users' Amundsen implementations */
-
-    return [
-      <div className="home-element-container">
-        <SearchBarWidget />
-      </div>,
-      <div className="filter-breadcrumb pull-right">
-        <Breadcrumb
-          direction="right"
-          path="/search"
-          text={SEARCH_BREADCRUMB_TEXT}
-        />
-      </div>,
-      <div className="home-element-container">
-        <BadgesListWidget shortBadgesList />
-      </div>,
-      <div className="home-element-container">
-        <TagsListWidget shortTagsList />
-      </div>,
-      <div className="home-element-container">
-        <MyBookmarksWidget />
-      </div>,
-      <div className="home-element-container">
-        <PopularResourcesWidget />
-      </div>,
-    ];
-  }
-
-  defaultLayout: HomePageLayout = [];
-
-  homePageWidgets = this.getHomePageWidgets(this.defaultLayout);
 
   render() {
     /* TODO, just display either popular or curated tags,
@@ -84,7 +159,7 @@ export class HomePage extends React.Component<HomePageProps> {
             }`}
           >
             <h1 className="sr-only">{HOMEPAGE_TITLE}</h1>
-            {this.homePageWidgets}
+            <HomePageWidgets homePageLayout={defaultHomePageLayout} />
           </div>
           {announcementsEnabled() && (
             <div className="col-xs-12 col-md-offset-1 col-md-3">
