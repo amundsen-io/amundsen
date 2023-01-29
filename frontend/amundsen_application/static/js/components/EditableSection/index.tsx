c2954950d28c5720fed1a5d71e4e0917a77482d1
Marcos Iglesias <190833+Golodhros@users.noreply.github.com>
2023-01-28 20:40:55 -0800

    chore: updates betterer; cleans some eslint issues (#2088)
    * chore: deal with ESLint issues in EditableSection
    
    Signed-off-by: Marcos Iglesias <golodhros@gmail.com>
    
    * chore: updates betterer; cleans eslint issues
    
    Signed-off-by: Marcos Iglesias <golodhros@gmail.com>
    
    * chore: fix issue
    
    Signed-off-by: Marcos Iglesias <golodhros@gmail.com>
    
    * chore: betterer update
    
    Signed-off-by: Marcos Iglesias <golodhros@gmail.com>
    
    ---------
    
    Signed-off-by: Marcos Iglesias <golodhros@gmail.com>

--- frontend/amundsen_application/static/js/components/EditableSection/index.tsx
+++ frontend/amundsen_application/static/js/components/EditableSection/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 // Copyright Contributors to the Amundsen project.
 // SPDX-License-Identifier: Apache-2.0
 
@@ -35,6 +36,13 @@ export class EditableSection extends React.Component<
     editText: Constants.EDIT_TEXT,
   };
 
+  static convertText(str: string): string {
+    return str
+      .split(new RegExp('[\\s+_]'))
+      .map((x) => x.charAt(0).toUpperCase() + x.slice(1).toLowerCase())
+      .join(' ');
+  }
+
   constructor(props) {
     super(props);
 
@@ -48,36 +56,34 @@ export class EditableSection extends React.Component<
   };
 
   toggleEdit = () => {
-    this.setState({ isEditing: !this.state.isEditing });
+    const { isEditing } = this.state;
+
+    this.setState({ isEditing: !isEditing });
   };
 
   preventDefault = (event: React.MouseEvent<HTMLDivElement>) => {
     event.preventDefault();
   };
 
-  static convertText(str: string): string {
-    return str
-      .split(new RegExp('[\\s+_]'))
-      .map((x) => x.charAt(0).toUpperCase() + x.slice(1).toLowerCase())
-      .join(' ');
-  }
+  renderButton = (): React.ReactNode => {
+    const { isEditing } = this.state;
 
-  renderButton = (): React.ReactNode => (
-    <button
-      className={`btn btn-flat-icon edit-button ${
-        this.state.isEditing ? 'active' : ''
-      }`}
-      onClick={this.toggleEdit}
-    >
-      <span className="sr-only">{Constants.EDIT_TEXT}</span>
-      <img
-        className={`icon icon-small icon-edit ${
-          this.state.isEditing ? 'icon-color' : ''
-        }`}
-        alt=""
-      />
-    </button>
-  );
+    return (
+      <button
+        type="button"
+        className={`btn btn-flat-icon edit-button ${isEditing ? 'active' : ''}`}
+        onClick={this.toggleEdit}
+      >
+        <span className="sr-only">{Constants.EDIT_TEXT}</span>
+        <img
+          className={`icon icon-small icon-edit ${
+            isEditing ? 'icon-color' : ''
+          }`}
+          alt=""
+        />
+      </button>
+    );
+  };
 
   renderReadOnlyButton = (): React.ReactNode => {
     const { editText, editUrl } = this.props;
@@ -110,6 +116,8 @@ export class EditableSection extends React.Component<
 
   render() {
     const { children, title, readOnly = false } = this.props;
+    const { isEditing } = this.state;
+
     const childrenWithProps = React.Children.map(children, (child) => {
       if (!React.isValidElement(child)) {
         return child;
@@ -117,7 +125,7 @@ export class EditableSection extends React.Component<
 
       return React.cloneElement(child, {
         readOnly,
-        isEditing: this.state.isEditing,
+        isEditing,
         setEditMode: this.setEditMode,
       });
     });
