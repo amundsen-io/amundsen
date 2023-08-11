8c3ceb7f1b274daa8de9e842789beb21dfeae6d7
Ben Dye <bdye@lyft.com>
2023-08-11 12:11:07 -0400

    Refactor Neo4jProxy table owners query for easier customization (#2182)
    * refactor _exec_owners_query and get_table
    
    Signed-off-by: Ben Dye <bdye@lyft.com>
    
    * remove prior owners logic and references
    
    Signed-off-by: Ben Dye <bdye@lyft.com>
    
    * WIP
    
    Signed-off-by: Ben Dye <bdye@lyft.com>
    
    * update tests
    
    Signed-off-by: Ben Dye <bdye@lyft.com>
    
    * Add comment explaining testing-mock logic
    
    Signed-off-by: Ben Dye <bdye@lyft.com>
    
    * Cleanups
    
    Signed-off-by: Ben Dye <bdye@lyft.com>
    
    * Bump version
    
    Signed-off-by: Ben Dye <bdye@lyft.com>
    
    ---------
    
    Signed-off-by: Ben Dye <bdye@lyft.com>

--- metadata/metadata_service/proxy/neo4j_proxy.py
+++ metadata/metadata_service/proxy/neo4j_proxy.py
@@ -159,8 +159,9 @@ class Neo4jProxy(BaseProxy):
         cols, last_neo4j_record = self._exec_col_query(table_uri)
 
         readers = self._exec_usage_query(table_uri)
+        owners = self._exec_owners_query(table_uri)
 
-        wmk_results, table_writer, table_apps, timestamp_value, owners, tags, source, \
+        wmk_results, table_writer, table_apps, timestamp_value, tags, source, \
             badges, prog_descs, resource_reports = self._exec_table_query(table_uri)
 
         joins, filters = self._exec_table_query_query(table_uri)
@@ -341,13 +342,33 @@ class Neo4jProxy(BaseProxy):
         return readers
 
     @timer_with_counter
+    def _exec_owners_query(self, table_uri: str) -> List[User]:
+        # Return Value: List[User]
+        owners_query = textwrap.dedent("""
+            MATCH (owner:User)<-[:OWNER]-(tbl:Table {key: $tbl_key})
+            RETURN collect(distinct owner) as owner_records
+        """)
+        owners_neo4j_records = self._execute_cypher_query(statement=owners_query,
+                                                          param_dict={'tbl_key': table_uri})
+
+        owners_neo4j_records = get_single_record(owners_neo4j_records)
+
+        owners = []  # type: List[User]
+        for owner_neo4j_record in owners_neo4j_records.get('owner_records', []):
+            owner_data = self._get_user_details(user_id=owner_neo4j_record['email'])
+            owner = self._build_user_from_record(record=owner_data)
+            owners.append(owner)
+
+        return owners
+
+    @timer_with_counter
     def _exec_table_query(self, table_uri: str) -> Tuple:
         """
         Queries one Cypher record with watermark list, Application,
-        ,timestamp, owner records and tag records.
+        ,timestamp, and tag records.
         """
 
-        # Return Value: (Watermark Results, Table Writer, Last Updated Timestamp, owner records, tag records)
+        # Return Value: (Watermark Results, Table Writer, Last Updated Timestamp, tag records)
 
         table_level_query = textwrap.dedent("""\
         MATCH (tbl:Table {key: $tbl_key})
@@ -355,7 +376,6 @@ class Neo4jProxy(BaseProxy):
         OPTIONAL MATCH (app_producer:Application)-[:GENERATES]->(tbl)
         OPTIONAL MATCH (app_consumer:Application)-[:CONSUMES]->(tbl)
         OPTIONAL MATCH (tbl)-[:LAST_UPDATED_AT]->(t:Timestamp)
-        OPTIONAL MATCH (owner:User)<-[:OWNER]-(tbl)
         OPTIONAL MATCH (tbl)-[:TAGGED_BY]->(tag:Tag{tag_type: $tag_normal_type})
         OPTIONAL MATCH (tbl)-[:HAS_BADGE]->(badge:Badge)
         OPTIONAL MATCH (tbl)-[:SOURCE]->(src:Source)
@@ -365,7 +385,6 @@ class Neo4jProxy(BaseProxy):
         collect(distinct app_producer) as producing_apps,
         collect(distinct app_consumer) as consuming_apps,
         t.last_updated_timestamp as last_updated_timestamp,
-        collect(distinct owner) as owner_records,
         collect(distinct tag) as tag_records,
         collect(distinct badge) as badge_records,
         src,
@@ -405,12 +424,6 @@ class Neo4jProxy(BaseProxy):
 
         timestamp_value = table_records['last_updated_timestamp']
 
-        owner_record = []
-
-        for owner in table_records.get('owner_records', []):
-            owner_data = self._get_user_details(user_id=owner['email'])
-            owner_record.append(self._build_user_from_record(record=owner_data))
-
         src = None
 
         if table_records['src']:
@@ -423,7 +436,7 @@ class Neo4jProxy(BaseProxy):
 
         resource_reports = self._extract_resource_reports_from_query(table_records.get('resource_reports', []))
 
-        return wmk_results, table_writer, table_apps, timestamp_value, owner_record,\
+        return wmk_results, table_writer, table_apps, timestamp_value,\
             tags, src, badges, prog_descriptions, resource_reports
 
     @timer_with_counter
@@ -434,7 +447,7 @@ class Neo4jProxy(BaseProxy):
         on the table.
         """
 
-        # Return Value: (Watermark Results, Table Writer, Last Updated Timestamp, owner records, tag records)
+        # Return Value: (Watermark Results, Table Writer, Last Updated Timestamp, tag records)
         table_query_level_query = textwrap.dedent("""
         MATCH (tbl:Table {key: $tbl_key})
         OPTIONAL MATCH (tbl)-[:COLUMN]->(col:Column)-[COLUMN_JOINS_WITH]->(j:Join)
