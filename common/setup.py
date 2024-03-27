5b3bafb174c28b9d919c9623d24812f75996098f
Kristen Armes <karmes@lyft.com>
2024-03-27 14:43:03 -0700

    Add py3.10 to compatibility matrix
    Signed-off-by: Kristen Armes <karmes@lyft.com>

--- common/setup.py
+++ common/setup.py
@@ -51,5 +51,6 @@ setup(
     classifiers=[
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
 )
