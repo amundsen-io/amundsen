7846ff25d47f02ff1da4ed79eae0e23dff9dca8e
Jackson Goerner <jgoerner@squareup.com>
2024-12-10 15:06:19 +1100

    Fix + Add log tests.
    Signed-off-by: Jackson Goerner <jgoerner@squareup.com>

--- frontend/amundsen_application/static/js/ducks/log/index.spec.ts
+++ frontend/amundsen_application/static/js/ducks/log/index.spec.ts
@@ -5,11 +5,14 @@ import { testSaga } from 'redux-saga-test-plan';
 
 import { ResourceType } from 'interfaces/Resources';
 import * as Analytics from 'utils/analytics';
+import globalState from 'fixtures/globalState';
 import { logSearchEvent } from './reducer';
 
 import { logSearchEventWatcher, logSearchEventWorker } from './sagas';
 
 import { LogSearchEvent } from './types';
+
+const searchState = globalState.search;
 
 describe('log ducks', () => {
   const resourceLink = '/some/resource/link';
@@ -19,6 +22,12 @@ describe('log ducks', () => {
   const event = 'some_event';
   const inline = false;
   const extra = { key: 'value', other_key: 42 };
+
+  const mockState = {
+    search: {
+      ...searchState,
+    },
+  };
 
   describe('actions', () => {
     it('logSearchEvent - returns the action to log search events', () => {
@@ -70,14 +79,16 @@ describe('log ducks', () => {
           )
         )
           .next()
+          .select()
+          .next(mockState)
           .call(Analytics.logClick, event, {
             value: source,
             position: index.toString(),
             resource_href: resourceLink,
             resource_type: resourceType,
-            search_page_index: expect.any(Number),
-            search_term: expect.any(String),
-            search_results: expect.any(Array),
+            search_page_index: mockState.search.tables.page_index,
+            search_term: mockState.search.search_term,
+            search_results: mockState.search.tables.results.map((t) => t.key),
             ...extra,
           })
           .next()
@@ -100,18 +111,88 @@ describe('log ducks', () => {
           )
         )
           .next()
+          .select()
+          .next(mockState)
           .call(Analytics.logClick, event, {
             value: source,
             position: index.toString(),
             resource_href: resourceLink,
             resource_type: resourceType,
-            search_page_index: expect.any(Number),
-            search_term: expect.any(String),
-            search_results: expect.any(Array),
+            search_page_index: mockState.search.tables.page_index,
+            search_term: mockState.search.search_term,
+            search_results: mockState.search.tables.results.map((t) => t.key),
             ...extra,
           })
           .throw(new Error('error'))
           .put({ type: LogSearchEvent.FAILURE, payload: { completed: false } })
+          .next()
+          .isDone();
+      });
+
+      it('handles resource types correctly', () => {
+        testSaga(
+          logSearchEventWorker,
+          logSearchEvent(
+            resourceLink,
+            ResourceType.dashboard,
+            source,
+            index,
+            event,
+            inline,
+            extra
+          )
+        )
+          .next()
+          .select()
+          .next(mockState)
+          .call(Analytics.logClick, event, {
+            value: source,
+            position: index.toString(),
+            resource_href: resourceLink,
+            resource_type: ResourceType.dashboard,
+            search_page_index: mockState.search.dashboards.page_index,
+            search_term: mockState.search.search_term,
+            search_results: mockState.search.dashboards.results.map(
+              (d) => d.uri
+            ),
+            ...extra,
+          })
+          .next()
+          .put({ type: LogSearchEvent.SUCCESS, payload: { completed: true } })
+          .next()
+          .isDone();
+      });
+
+      it('handles inline search events correctly', () => {
+        testSaga(
+          logSearchEventWorker,
+          logSearchEvent(
+            resourceLink,
+            resourceType,
+            source,
+            index,
+            event,
+            true,
+            extra
+          )
+        )
+          .next()
+          .select()
+          .next(mockState)
+          .call(Analytics.logClick, event, {
+            value: source,
+            position: index.toString(),
+            resource_href: resourceLink,
+            resource_type: resourceType,
+            search_page_index: mockState.search.inlineResults.tables.page_index,
+            search_term: mockState.search.search_term,
+            search_results: mockState.search.inlineResults.tables.results.map(
+              (t) => t.key
+            ),
+            ...extra,
+          })
+          .next()
+          .put({ type: LogSearchEvent.SUCCESS, payload: { completed: true } })
           .next()
           .isDone();
       });
