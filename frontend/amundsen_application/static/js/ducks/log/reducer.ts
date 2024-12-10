9c1dbc462d4bcffe7d011a166056f75cd9b841e7
Jackson Goerner <jgoerner@squareup.com>
2024-12-10 11:15:31 +1100

    Add search logSearchEvent function.
    Signed-off-by: Jackson Goerner <jgoerner@squareup.com>

---
+++ frontend/amundsen_application/static/js/ducks/log/reducer.ts
@@ -0,0 +1,25 @@
+import { ResourceType } from 'interfaces/Resources';
+import { LogSearchEvent, LogSearchEventRequest } from './types';
+
+export function logSearchEvent(
+  resourceLink: string,
+  resourceType: ResourceType,
+  source: string,
+  index: number,
+  event: any,
+  inline: boolean,
+  extra?: { [key: string]: any }
+): LogSearchEventRequest {
+  return {
+    type: LogSearchEvent.REQUEST,
+    payload: {
+      resourceLink,
+      resourceType,
+      event,
+      source,
+      index,
+      inline,
+      extra,
+    },
+  };
+}
